d45e011b74674362b7c2e93c173213f29368f5e7
Alexandru Nedelcu <alex@bionicspirit.com>
2014-01-20 10:48:09 +0200

    Rx

--- core/src/main/scala/monifu/rx/Observable.scala
+++ core/src/main/scala/monifu/rx/Observable.scala
@@ -2,31 +2,20 @@ package monifu.rx
 
 import scala.concurrent.duration.FiniteDuration
 import monifu.concurrent.atomic.Atomic
-import monifu.concurrent.Scheduler
 import scala.concurrent.ExecutionContext
-
-trait Observer[-T] {
-  def onNext(elem: T): Unit
-  def onError(ex: Throwable): Unit
-  def onComplete(): Unit  
-}
 
 trait Observable[+A] {
   def subscribe(observer: Observer[A]): Subscription
 
   def subscribe(f: A => Unit): Subscription = 
-    subscribe(new Observer[A] {
-      def onNext(elem: A) = f(elem)
-      def onError(ex: Throwable) = throw ex
-      def onComplete(): Unit = {}
-    })
+    subscribe(
+      onNext = (elem: A) => f(elem),
+      onError = (ex: Throwable) => throw ex,
+      onComplete = () => {}
+    )
 
-  def subscribe(next: A => Unit, error: Throwable => Unit, complete: () => Unit): Subscription = 
-    subscribe(new Observer[A] {
-      def onNext(elem: A) = next(elem)
-      def onError(ex: Throwable) = error(ex)
-      def onComplete() = complete()
-    })
+  def subscribe(onNext: A => Unit, onError: Throwable => Unit, onComplete: () => Unit): Subscription = 
+    subscribe(Observer(onNext, onError, onComplete))
 
   def map[B](f: A => B): Observable[B] = 
     Observable[B](observer => subscribe(
@@ -39,23 +28,22 @@ trait Observable[+A] {
     Observable[B] { observer => 
       val composite = CompositeSubscription()
 
-      composite += subscribe(new Observer[A] {
-        def onNext(elem: A) = 
+      composite += subscribe(
+        onError = observer.onError,
+        onComplete = observer.onComplete,
+        onNext = (elem: A) => 
           composite += f(elem).subscribe(observer)
-        def onError(ex: Throwable) = 
-          observer.onError(ex)
-        def onComplete() = 
-          observer.onComplete()
-      })
+      )
 
       composite
     }
 
   def filter(p: A => Boolean): Observable[A] =
     Observable(observer => subscribe(
-      (elem: A) => if (p(elem)) observer.onNext(elem),
-      (ex: Throwable) => observer.onError(ex),
-      () => observer.onComplete()
+      onError = observer.onError,
+      onComplete = observer.onComplete,
+      onNext = (elem: A) => 
+        if (p(elem)) observer.onNext(elem)
     ))
 
   def startWith[B >: A](elems: B*): Observable[B] = 
@@ -94,14 +82,11 @@ object Observable {
   def interval(period: FiniteDuration)(implicit s: Scheduler, ec: ExecutionContext): Observable[Long] =
     Observable { observer =>
       val counter = Atomic(0L)
-      val task = s.schedule(period, period) {
+
+      s.schedule(period, period, {
         val nr = counter.incrementAndGet()
         observer.onNext(nr)
-      }
-
-      Subscription {
-        task.cancel()
-      }
+      })
     }
 }
 
