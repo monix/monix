0950f7ad23659ae6bdfab88e992b617e98a63987
Alexandru Nedelcu <noreply@alexn.org>
2014-03-26 23:18:58 +0200

    Project reorg

---
+++ monifu-core-js/src/main/scala/monifu/concurrent/atomic/package.scala
@@ -0,0 +1,59 @@
+package monifu.concurrent
+
+package object atomic {
+  type AtomicFloat = AtomicNumberAny[Float]
+
+  object AtomicFloat {
+    def apply(initial: Float): AtomicFloat =
+      AtomicNumberAny(initial)
+  }
+
+  type AtomicDouble = AtomicNumberAny[Double]
+
+  object AtomicDouble {
+    def apply(initial: Double): AtomicDouble  =
+      AtomicNumberAny(initial)
+  }
+
+  type AtomicShort = AtomicNumberAny[Short]
+
+  object AtomicShort {
+    def apply(initial: Short): AtomicShort  =
+      AtomicNumberAny(initial)
+  }
+
+  type AtomicChar = AtomicNumberAny[Char]
+
+  object AtomicChar {
+    def apply(initial: Char): AtomicChar  =
+      AtomicNumberAny(initial)
+  }
+
+  type AtomicBoolean = AtomicAny[Boolean]
+
+  object AtomicBoolean {
+    def apply(initial: Boolean): AtomicBoolean  =
+      AtomicAny(initial)
+  }
+
+  type AtomicInt = AtomicNumberAny[Int]
+
+  object AtomicInt {
+    def apply(initial: Int): AtomicInt  =
+      AtomicNumberAny(initial)
+  }
+
+  type AtomicLong = AtomicNumberAny[Long]
+
+  object AtomicLong {
+    def apply(initial: Long): AtomicLong  =
+      AtomicNumberAny(initial)
+  }
+
+  type AtomicByte = AtomicNumberAny[Byte]
+
+  object AtomicByte {
+    def apply(initial: Byte): AtomicByte  =
+      AtomicNumberAny(initial)
+  }
+}
