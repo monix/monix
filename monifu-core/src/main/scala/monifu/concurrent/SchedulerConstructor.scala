556335235982ae4680a2b641044ff1066ee5676c
Alexandru Nedelcu <noreply@alexn.org>
2014-03-28 06:56:59 +0200

    Add tests for BooleanCancelable and MultiAssignmentCancelable

--- monifu-core/src/main/scala/monifu/concurrent/SchedulerConstructor.scala
+++ monifu-core/src/main/scala/monifu/concurrent/SchedulerConstructor.scala
@@ -3,6 +3,6 @@ package monifu.concurrent
 import scala.concurrent.ExecutionContext
 
 private[concurrent] trait SchedulerConstructor {
-  def asyncScheduler(implicit ec: ExecutionContext): Scheduler =
+  def concurrent(implicit ec: ExecutionContext): Scheduler =
     ConcurrentScheduler(ec)
 }
