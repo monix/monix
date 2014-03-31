45231de425ba711b8bc30c366a764df494f76ee8
Alexandru Nedelcu <noreply@alexn.org>
2014-03-31 14:16:26 +0300

    Refactoring / add PublishSubject

--- monifu-rx/src/shared/scala/monifu/rx/SafeObserver.scala
+++ monifu-rx/src/shared/scala/monifu/rx/SafeObserver.scala
@@ -1,78 +1,35 @@
 package monifu.rx
 
 import monifu.concurrent.Cancelable
-import monifu.concurrent.atomic.Atomic
-import scala.annotation.tailrec
-
+import monifu.concurrent.locks.ReadWriteLock
 
 final class SafeObserver[-T] private (observer: Observer[T], cancelable: Cancelable) extends Observer[T] {
-  private[this] val IDLE = 0
-  private[this] val NEXT = 1
-  private[this] val DONE = 2
+  private[this] val lock = ReadWriteLock()
+  private[this] var isDone = false
 
-  @volatile
-  private[this] var writeBarrier = false
-  private[this] val state = Atomic(IDLE)
+  def onNext(elem: T): Unit =
+    lock.readLock {
+      if (!isDone)
+        observer.onNext(elem)
+    }
 
-  @tailrec
-  def onNext(elem: T) = state.get match {
-    case IDLE =>
-      if (state.compareAndSet(IDLE, NEXT))
-        try {
-          observer.onNext(elem)
-        }
-        finally {
-          state.set(IDLE)
-        }
-      else
-        onNext(elem)
-    case NEXT =>
-      onNext(elem)
-    case DONE =>
-      // do nothing
-  }
-
-  @tailrec
-  def onError(ex: Throwable) = state.get match {
-    case IDLE =>
-      if (state.compareAndSet(IDLE, DONE))
-        try {
-          observer.onError(ex)
-          writeBarrier = !writeBarrier
-        }
-        finally {
+  def onError(ex: Throwable): Unit =
+    lock.writeLock {
+      if (!isDone)
+        try observer.onError(ex) finally {
+          isDone = true
           cancelable.cancel()
         }
-      else
-        // retry
-        onError(ex)
-    case NEXT =>
-      // retry
-      onError(ex)
-    case DONE =>
-      // do nothing
-  }
+    }
 
-  @tailrec
-  def onCompleted() = state.get match {
-    case IDLE =>
-      if (state.compareAndSet(IDLE, DONE))
-        try {
-          observer.onCompleted()
-          writeBarrier = !writeBarrier
-        }
-        finally {
+  def onCompleted(): Unit =
+    lock.writeLock {
+      if (!isDone)
+        try observer.onCompleted() finally {
+          isDone = true
           cancelable.cancel()
         }
-      else
-        // retry
-        onCompleted()
-    case NEXT =>
-      // retry
-      onCompleted()
-    case DONE =>
-    // do nothing
-  }
+    }
 }
 
 object SafeObserver {
---
+++ monifu-rx/src/shared/scala/monifu/rx/subjects/PublishSubject.scala
@@ -0,0 +1,55 @@
+package monifu.rx.subjects
+
+import monifu.rx.{Observable, Observer}
+import monifu.concurrent.Cancelable
+import monifu.concurrent.locks.ReadWriteLock
+import monifu.concurrent.cancelables.{CompositeCancelable, BooleanCancelable}
+import collection.immutable.Set
+
+final class PublishSubject[T] private () extends Observable[T] with Observer[T] {
+  private[this] val lock = ReadWriteLock()
+  private[this] var observers = Set.empty[Observer[T]]
+  private[this] val composite = CompositeCancelable()
+  private[this] var isDone = false
+
+  protected def fn(observer: Observer[T]): Cancelable =
+    lock.writeLock {
+      if (!isDone) {
+        observers = observers + observer
+        val sub = BooleanCancelable {
+          observers = observers - observer
+        }
+
+        composite += sub
+        sub
+      }
+      else
+        BooleanCancelable.alreadyCanceled
+    }
+
+  def onNext(elem: T): Unit = lock.readLock {
+    if (!isDone)
+      observers.foreach(_.onNext(elem))
+  }
+
+  def onError(ex: Throwable): Unit = lock.writeLock {
+    if (!isDone) {
+      isDone = true
+      observers.foreach(_.onError(ex))
+      composite.cancel()
+    }
+  }
+
+  def onCompleted(): Unit = lock.writeLock {
+    if (!isDone) {
+      isDone = true
+      observers.foreach(_.onCompleted())
+      composite.cancel()
+    }
+  }
+}
+
+object PublishSubject {
+  def apply[T](): PublishSubject[T] =
+    new PublishSubject[T]
+}
