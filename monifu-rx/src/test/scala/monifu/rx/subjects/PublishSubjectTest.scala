7a31fa2da628c55e0df82b262882aa7346a3ea2e
Alexandru Nedelcu <noreply@alexn.org>
2014-04-01 17:44:11 +0300

    Fix test

--- monifu-rx/src/test/scala/monifu/rx/subjects/PublishSubjectTest.scala
+++ monifu-rx/src/test/scala/monifu/rx/subjects/PublishSubjectTest.scala
@@ -1,7 +1,32 @@
 package monifu.rx.subjects
 
 import org.scalatest.FunSuite
+import monifu.concurrent.cancelables.BooleanCancelable
 
 class PublishSubjectTest extends FunSuite {
+  test("propagate onNext") {
+    val subject = PublishSubject[Int]()
 
+    var sum = 0
+    subject.filter(_ % 2 == 0).map(_ * 2).subscribe(x => sum += x)
+    subject.filter(_ % 2 == 1).map(_ * 2).subscribe(x => sum += x)
+
+    for (i <- 1 to 10)
+      subject.onNext(i)
+    subject.onCompleted()
+
+    assert(sum === 5 * 11 * 2)
+  }
+
+  test("cancel subscriptions onTerminated") {
+    val subject = PublishSubject[Int]()
+    val sub1 = subject.filter(_ % 2 == 0).map(_ * 2).subscribe(_ => ())
+    val sub2 = subject.filter(_ => true).subscribe(_ => ())
+
+    subject.onNext(1)
+    subject.onCompleted()
+
+    assert(sub1.asInstanceOf[BooleanCancelable].isCanceled === true)
+    assert(sub2.asInstanceOf[BooleanCancelable].isCanceled === true)
+  }
 }
