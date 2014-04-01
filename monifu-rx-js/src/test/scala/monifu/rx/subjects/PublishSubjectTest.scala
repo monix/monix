deb8d90a4c5aed655d98785ec89927048bc975f2
Alexandru Nedelcu <alex@bionicspirit.com>
2014-04-01 17:44:11 +0300

    Fix test

--- monifu-rx-js/src/test/scala/monifu/rx/subjects/PublishSubjectTest.scala
+++ monifu-rx-js/src/test/scala/monifu/rx/subjects/PublishSubjectTest.scala
@@ -22,11 +22,10 @@ object PublishSubjectTest extends JasmineTest {
     it("should cancel subscriptions when terminated") {
       val subject = PublishSubject[Int]()
       val sub1 = subject.filter(_ % 2 == 0).map(_ * 2).subscribe(_ => ())
+      val sub2 = subject.filter(_ => true).subscribe(_ => ())
 
       subject.onNext(1)
       subject.onCompleted()
-
-      val sub2 = subject.filter(_ => true).subscribe(_ => ())
 
       expect(sub1.asInstanceOf[BooleanCancelable].isCanceled).toBe(true)
       expect(sub2.asInstanceOf[BooleanCancelable].isCanceled).toBe(true)
