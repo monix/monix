14d4d3246df3f0a709e9cbceb4101aef52c8e928
Alexandru Nedelcu <noreply@alexn.org>
2014-04-07 14:22:38 +0300

    Add doOnCancel + doWork

--- monifu-rx/src/shared/scala/monifu/rx/observers/Subscriber.scala
+++ monifu-rx/src/shared/scala/monifu/rx/observers/Subscriber.scala
@@ -3,24 +3,27 @@ package monifu.rx.observers
 import monifu.concurrent.Cancelable
 import monifu.concurrent.cancelables.CompositeCancelable
 
-final class Subscriber[-T] private (observer: Observer[T], private val subscription: CompositeCancelable)
+final class Subscriber[-T] private (observer: Observer[T], subscriptions: CompositeCancelable)
   extends Observer[T] with CompositeCancelable {
 
   def onNext(elem: T): Unit = observer.onNext(elem)
   def onError(ex: Throwable): Unit = observer.onError(ex)
   def onCompleted(): Unit = observer.onCompleted()
 
-  def isCanceled: Boolean = subscription.isCanceled
-  def cancel(): Unit = subscription.cancel()
+  def isCanceled: Boolean =
+    subscriptions.isCanceled
+
+  def cancel(): Unit =
+    subscriptions.cancel()
 
   def add(s: Cancelable): Unit =
-    if (s != this) subscription.add(s)
+    if (s != this) subscriptions.add(s)
 
   def remove(s: Cancelable): Unit =
-    if (s != this) subscription.remove(s)
+    subscriptions.remove(s)
 
   def map[U](f: Observer[T] => Observer[U]): Subscriber[U] =
-    new Subscriber[U](f(observer), subscription)
+    new Subscriber[U](f(observer), subscriptions)
 }
 
 object Subscriber {
