06be238c270fd9924fc0f35259c3b9f869a07671
Alexandru Nedelcu <alex@bionicspirit.com>
2014-04-23 15:34:24 +0300

    Release 0.6.1

--- project/Build.scala
+++ project/Build.scala
@@ -9,13 +9,23 @@ object Build extends SbtBuild {
 
   val sharedSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.monifu",
-    version := "0.6",
+    version := "0.6.1",
     scalaVersion := "2.10.4",
     crossScalaVersions := Seq("2.10.4", "2.11.0"),
 
+    initialize := {
+       val _ = initialize.value // run the previous initialization
+       val classVersion = sys.props("java.class.version")
+       val specVersion = sys.props("java.specification.version")
+       assert(
+        classVersion.toDouble >= 50 && specVersion.toDouble >= 1.6,
+        s"JDK version 6 or newer is required for building this project " + 
+        s"(SBT instance running on top of JDK $specVersion with class version $classVersion)")
+    },
+
     scalacOptions ++= Seq(
       "-unchecked", "-deprecation", "-feature", "-Xlint", "-target:jvm-1.6", "-Yinline-warnings",
-      "–Xfatal-warnings", "-optimise", "-Ywarn-adapted-args", "-Ywarn-dead-code", "-Ywarn-inaccessible",
+      "-optimise", "-Ywarn-adapted-args", "-Ywarn-dead-code", "-Ywarn-inaccessible",
       "-Ywarn-nullary-override", "-Ywarn-nullary-unit"
     ),
 
