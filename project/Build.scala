7e078067076ab73cccd323f1ccd6f57176eec7b9
Alexandru Nedelcu <noreply@alexn.org>
2014-06-09 15:49:57 +0300

    Version bump

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import scala.scalajs.sbtplugin.ScalaJSPlugin.ScalaJSKeys._
 object Build extends SbtBuild {
   val sharedSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.monifu",
-    version := "0.13.0-M4",
+    version := "0.13.0-RC1",
     scalaVersion := "2.11.1",
 
     initialize := {
