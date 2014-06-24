1a5eddea957ceee7c6601fcc54a600cf134f30cf
Alexandru Nedelcu <noreply@alexn.org>
2014-06-24 10:10:36 +0300

    Fix SubscriberAsObserver implementation

--- monifu-rx/src/test/scala/monifu/reactive/streams/ObserverAsSubscriberTest.scala
+++ monifu-rx/src/test/scala/monifu/reactive/streams/ObserverAsSubscriberTest.scala
@@ -16,12 +16,13 @@
 
 package monifu.reactive.streams
 
-import java.util.concurrent.{TimeUnit, CountDownLatch}
+import java.util.concurrent.{CountDownLatch, TimeUnit}
 
-import monifu.reactive.Ack.Continue
-import monifu.reactive.{Observable, Ack, Observer}
-import org.scalatest.FunSpec
 import monifu.concurrent.Scheduler.Implicits.global
+import monifu.reactive.Ack.{Cancel, Continue}
+import monifu.reactive.{Observable, Observer}
+import org.scalatest.FunSpec
+
 import scala.concurrent.Future
 
 
@@ -33,7 +34,7 @@ class ObserverAsSubscriberTest extends FunSpec {
       val completed = new CountDownLatch(1)
 
       val subscriber = ObserverAsSubscriber(new Observer[Int] {
-        def onNext(elem: Int): Future[Ack] = {
+        def onNext(elem: Int) = {
           sum += elem
           Continue
         }
@@ -51,6 +52,307 @@ class ObserverAsSubscriberTest extends FunSpec {
 
       assert(completed.await(10, TimeUnit.SECONDS), "completed.await should have succeeded")
       assert(sum === 50000 * 100001)
+    }
+
+    it("should work with asynchronous boundaries and batched requests") {
+      for (_ <- 0 until 100) {
+        val completed = new CountDownLatch(1)
+        var sum = 0
+
+        val observer = new Observer[Int] {
+          def onNext(elem: Int) =
+            Future {
+              sum += elem
+              Continue
+            }
+
+          def onError(ex: Throwable): Unit = {
+            throw ex
+          }
+
+          def onComplete(): Unit = {
+            completed.countDown()
+          }
+        }
+
+        Observable.from(1 to 10000)
+          .subscribe(Subscriber.from(observer, requestSize = 128))
+
+        assert(completed.await(5, TimeUnit.SECONDS), "completed.await should have succeeded")
+        assert(sum === 5000 * 10001)
+      }
+    }
+
+    it("should work with asynchronous boundaries and requests of size 1") {
+      for (_ <- 0 until 100) {
+        val completed = new CountDownLatch(1)
+        var sum = 0
+
+        val observer = new Observer[Int] {
+          def onNext(elem: Int) =
+            Future {
+              sum += elem
+              Continue
+            }
+
+          def onError(ex: Throwable): Unit = {
+            throw ex
+          }
+
+          def onComplete(): Unit = {
+            completed.countDown()
+          }
+        }
+
+        Observable.from(1 to 10000)
+          .subscribe(Subscriber.from(observer, requestSize = 1))
+
+        assert(completed.await(5, TimeUnit.SECONDS), "completed.await should have succeeded")
+        assert(sum === 5000 * 10001)
+      }
+    }
+
+    it("should work synchronously and with batched requests") {
+      for (_ <- 0 until 100) {
+        val completed = new CountDownLatch(1)
+        var sum = 0
+
+        val observer = new Observer[Int] {
+          def onNext(elem: Int) ={
+            sum += elem
+            Continue
+          }
+
+          def onError(ex: Throwable): Unit = {
+            throw ex
+          }
+
+          def onComplete(): Unit = {
+            completed.countDown()
+          }
+        }
+
+        Observable.from(1 to 10000)
+          .subscribe(Subscriber.from(observer, requestSize = 128))
+
+        assert(completed.await(5, TimeUnit.SECONDS), "completed.await should have succeeded")
+        assert(sum === 5000 * 10001)
+      }
+    }
+
+    it("should work synchronously and with requests of size 1") {
+      for (_ <- 0 until 100) {
+        val completed = new CountDownLatch(1)
+        var sum = 0
+
+        val observer = new Observer[Int] {
+          def onNext(elem: Int) = {
+            sum += elem
+            Continue
+          }
+
+          def onError(ex: Throwable): Unit = {
+            throw ex
+          }
+
+          def onComplete(): Unit = {
+            completed.countDown()
+          }
+        }
+
+        Observable.from(1 to 10000)
+          .subscribe(Subscriber.from(observer, requestSize = 1))
+
+        assert(completed.await(5, TimeUnit.SECONDS), "completed.await should have succeeded")
+        assert(sum === 5000 * 10001)
+      }
+    }
+
+    it("should do flatMap with batched requests") {
+      for (_ <- 0 until 100) {
+        val completed = new CountDownLatch(1)
+        var sum = 0
+
+        val observer = new Observer[Int] {
+          def onNext(elem: Int) = {
+            sum += elem
+            Continue
+          }
+
+          def onError(ex: Throwable): Unit = {
+            throw ex
+          }
+
+          def onComplete(): Unit = {
+            completed.countDown()
+          }
+        }
+
+        Observable.from(1 to 10000).flatMap(x => Observable.unit(x))
+          .subscribe(Subscriber.from(observer, requestSize = 128))
+
+        assert(completed.await(5, TimeUnit.SECONDS), "completed.await should have succeeded")
+        assert(sum === 5000 * 10001)
+      }
+    }
+
+    it("should do flatMap with requests of size 1") {
+      for (_ <- 0 until 100) {
+        val completed = new CountDownLatch(1)
+        var sum = 0
+
+        val observer = new Observer[Int] {
+          def onNext(elem: Int) = {
+            sum += elem
+            Continue
+          }
+
+          def onError(ex: Throwable): Unit = {
+            throw ex
+          }
+
+          def onComplete(): Unit = {
+            completed.countDown()
+          }
+        }
+
+        Observable.from(1 to 10000).flatMap(x => Observable.unit(x))
+          .subscribe(Subscriber.from(observer, requestSize = 1))
+
+        assert(completed.await(5, TimeUnit.SECONDS), "completed.await should have succeeded")
+        assert(sum === 5000 * 10001)
+      }
+    }
+
+    it("should do mergeMap with batched requests") {
+      for (_ <- 0 until 100) {
+        val completed = new CountDownLatch(1)
+        var sum = 0
+
+        val observer = new Observer[Int] {
+          def onNext(elem: Int) = {
+            sum += elem
+            Continue
+          }
+
+          def onError(ex: Throwable): Unit = {
+            throw ex
+          }
+
+          def onComplete(): Unit = {
+            completed.countDown()
+          }
+        }
+
+        Observable.from(1 to 10000).mergeMap(x => Observable.unit(x))
+          .subscribe(Subscriber.from(observer, requestSize = 128))
+
+        assert(completed.await(5, TimeUnit.SECONDS), "completed.await should have succeeded")
+        assert(sum === 5000 * 10001)
+      }
+    }
+
+    it("should do mergeMap with requests of size 1") {
+      for (_ <- 0 until 100) {
+        val completed = new CountDownLatch(1)
+        var sum = 0
+
+        val observer = new Observer[Int] {
+          def onNext(elem: Int) = {
+            sum += elem
+            Continue
+          }
+
+          def onError(ex: Throwable): Unit = {
+            throw ex
+          }
+
+          def onComplete(): Unit = {
+            completed.countDown()
+          }
+        }
+
+        Observable.from(1 to 10000).mergeMap(x => Observable.unit(x))
+          .subscribe(Subscriber.from(observer, requestSize = 1))
+
+        assert(completed.await(5, TimeUnit.SECONDS), "completed.await should have succeeded")
+        assert(sum === 5000 * 10001)
+      }
+    }
+
+    it("should cancel precisely, test 1") {
+      for (_ <- 0 until 10000) {
+        val completed = new CountDownLatch(1)
+        var sum = 0
+
+        val observer = new Observer[Int] {
+          private[this] var received = 0
+
+          def onNext(elem: Int) = Future {
+            received += 1
+            sum += elem
+            if (received < 10) Continue
+            else if (received == 10) {
+              completed.countDown()
+              Cancel
+            }
+            else
+              throw new IllegalStateException(s"onNext($elem)")
+          }
+
+          def onError(ex: Throwable): Unit = {
+            throw ex
+          }
+
+          def onComplete(): Unit = {
+            throw new IllegalStateException("onComplete")
+          }
+        }
+
+        Observable.from(1 to 10000)
+          .subscribe(Subscriber.from(observer, requestSize = 1))
+
+        assert(completed.await(5, TimeUnit.SECONDS), "completed.await should have succeeded")
+        assert(sum === 5 * 11)
+      }
+    }
+
+    it("should cancel precisely, test 2") {
+      for (_ <- 0 until 10000) {
+        val completed = new CountDownLatch(1)
+        var sum = 0
+
+        val observer = new Observer[Int] {
+          private[this] var received = 0
+
+          def onNext(elem: Int) = {
+            received += 1
+            sum += elem
+            if (received < 100)
+              Continue
+            else if (received == 100) {
+              completed.countDown()
+              Cancel
+            }
+            else
+              throw new IllegalStateException(s"onNext($elem)")
+          }
+
+          def onError(ex: Throwable): Unit = {
+            throw ex
+          }
+
+          def onComplete(): Unit = {
+            throw new IllegalStateException("onComplete")
+          }
+        }
+
+        Observable.from(1 to 10000)
+          .subscribe(Subscriber.from(observer, requestSize = 1))
+
+        assert(completed.await(5, TimeUnit.SECONDS), "completed.await should have succeeded")
+        assert(sum === 50 * 101)
+      }
     }
   }
 }
