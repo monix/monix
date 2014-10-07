28842e0f29df3826be4a62507608f7790129d47d
Alexandru Nedelcu <noreply@alexn.org>
2014-10-07 11:22:01 +0300

    Cleanup / unification

---
+++ jvm/monifu-rx/src/main/scala/monifu/reactive/operators/delay.scala
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) 2014 by its authors. Some rights reserved.
+ * See the project homepage at
+ *
+ *     http://www.monifu.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monifu.reactive.operators
+
+import monifu.concurrent.cancelables.{BooleanCancelable, SingleAssignmentCancelable}
+import monifu.concurrent.{Scheduler, Cancelable}
+import monifu.reactive.Ack.{Cancel, Continue}
+import monifu.reactive.BufferPolicy.{BackPressured, OverflowTriggering, Unbounded}
+import monifu.reactive.internals._
+import monifu.reactive._
+
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Success}
+
+
+object delay {
+  /**
+   * Implementation for `Observable.delayFirst(policy, init)`.
+   */
+  def onEvent[T](source: Observable[T], policy: BufferPolicy)
+      (eventInit: (() => Unit, Throwable => Unit) => Cancelable)
+      (implicit s: Scheduler) = Observable.create[T] { observer =>
+    source.unsafeSubscribe(new Observer[T] {
+      private[this] val lock = new AnyRef
+      // MUST BE synchronized by lock at all times
+      private[this] var buffer = ArrayBuffer.empty[T]
+      // SHOULD BE synchronized by lock if false, if true then
+      // the logic can take the fast path and avoid lock synchronization
+      private[this] var isConnected = false
+      // signals that the downstream observer has canceled the stream
+      // during buffer draining, set to true after isConnected = true and
+      // before connectedF is completed
+      private[this] var isCanceledDuringFeeding = false
+      // Promise to be completed after the observer has been fed
+      // with the whole buffer
+      private[this] val connectedP = Promise[Ack]()
+      // Future to be completed after the observer has been fed
+      // with the whole buffer - reference must be mutable, because
+      // that flatMap-ing in onNext can become concurrent with
+      // onComplete / onError, so it must be changed to reflect
+      // the latest acknowledgement of `observer.onNext(elem)`
+      private[this] var connectedF = connectedP.future
+      // cancelable of the task that has to run for executing `connect`,
+      // initialized either in onNext or in onComplete.
+      private[this] val task = SingleAssignmentCancelable()
+      // value to be set to false after the first item being signaled
+      // accessed from onNext/onComplete so does not need synchronization
+      private[this] var isTaskInitialized = false
+
+      /** Initializes scheduled task, to be called in onNext/onComplete */
+      @inline private[this] def initializeTask(): Unit = {
+        if (!isTaskInitialized) {
+          task := eventInit(connect, onError)
+          isTaskInitialized = true
+        }
+      }
+
+      private[this] def connect(): Unit = lock.synchronized {
+        if (!isConnected) {
+          // signals that the observer has been connected to the stream
+          // and thus future events mustn't be buffered
+          isConnected = true
+
+          Observer.feed(observer, buffer).onCompleteNow {
+            case ack @ Success(Continue | Cancel) =>
+              lock.synchronized {
+                isCanceledDuringFeeding = ack == Cancel.IsSuccess
+                buffer = null // GC purposes
+                connectedP.complete(ack)
+              }
+            case Failure(ex) =>
+              isCanceledDuringFeeding = true
+              observer.onError(ex)
+              buffer = null // GC purposes
+              connectedP.success(Cancel)
+          }
+        }
+      }
+
+      def onNext(elem: T): Future[Ack] = {
+        if (isConnected && connectedF.isCompleted) {
+          // fast path branch taken when the observer is connected
+          // and the whole buffer has been drained
+          if (!isCanceledDuringFeeding) {
+            connectedF = observer.onNext(elem)
+            connectedF
+          }
+          else {
+            Cancel
+          }
+        }
+        else if (isConnected) {
+          // slow path branch taken when the observer is connected,
+          // but the buffer draining is in progress, so we must still do
+          // buffering, this time piggybacking on the execution context
+          connectedF = connectedF.flatMap {
+            case Cancel => Cancel
+            case Continue =>
+              observer.onNext(elem)
+          }
+          connectedF
+        }
+        else lock.synchronized {
+          // race condition guard
+          if (isConnected)
+            onNext(elem) // retry
+          else {
+            // initializing the task that will eventually connect
+            // the buffer to the downstream observer
+            initializeTask()
+
+            // we are still within the delay period and hence we are still
+            // buffering incoming events, but we can only do buffering
+            // according to the requested policy
+            policy match {
+              case Unbounded =>
+                // unbounded means buffer everything
+                buffer.append(elem)
+                Continue
+
+              case OverflowTriggering(capacity) =>
+                if (buffer.length < capacity) {
+                  buffer.append(elem)
+                  Continue
+                }
+                else {
+                  // if over capacity, then trigger error
+                  onError(new BufferOverflowException(
+                    s"Reached buffer capacity of $capacity during delay, " +
+                      "cannot buffer any more items"
+                  ))
+                  Cancel
+                }
+
+              case BackPressured(capacity) =>
+                if (buffer.length < capacity) {
+                  buffer.append(elem)
+                  Continue
+                }
+                else {
+                  // if over capacity, then do back-pressure
+                  connectedF = connectedF.flatMap {
+                    case Cancel => Cancel
+                    case Continue =>
+                      observer.onNext(elem)
+                  }
+                  connectedF
+                }
+            }
+          }
+        }
+      }
+
+      def onComplete(): Unit = {
+        initializeTask()
+        // the complete event gets scheduled to run after the
+        // observer has been connected and the buffer has
+        // been drained - careful here, since this logic can end up
+        // running concurrent with the flatMap-ing in onNext
+        connectedF.onContinueComplete(observer)
+      }
+
+      def onError(ex: Throwable): Unit = lock.synchronized {
+        if (!isConnected) {
+          // we are not connected yet, so cancel the buffer
+          // and stream the error immediately
+          task.cancel()
+          buffer = null // GC purposes
+          isConnected = true
+          observer.onError(ex)
+        }
+        else {
+          // we are connected, so we must stream the error only after
+          // the buffer has been drained - careful here, since this logic
+          // can end up running concurrently with the flatMap-ing in onNext
+          connectedF.onContinueComplete(observer, ex)
+        }
+      }
+    })
+  }
+
+  /**
+   * Implementation for `Observable.delayFirst(policy, future)`.
+   */
+  def onFuture[T](source: Observable[T], policy: BufferPolicy, future: Future[_])(implicit s: Scheduler) =
+      source.delayFirstOnEvent(policy = policy, eventInit = {
+        (connect, signalError) =>
+          val task = BooleanCancelable()
+          future.onComplete {
+            case Success(_) =>
+              if (!task.isCanceled)
+                connect()
+            case Failure(ex) =>
+              if (!task.isCanceled)
+                signalError(ex)
+          }
+
+          task
+      })
+}
---
+++ jvm/monifu-rx/src/main/scala/monifu/reactive/operators/take.scala
@@ -0,0 +1,292 @@
+/*
+ * Copyright (c) 2014 by its authors. Some rights reserved.
+ * See the project homepage at
+ *
+ *     http://www.monifu.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monifu.reactive.operators
+
+import monifu.concurrent.Scheduler
+import monifu.concurrent.atomic.AtomicBoolean
+import monifu.reactive.Ack.{Continue, Cancel}
+import monifu.reactive.observers.SynchronousObserver
+import monifu.reactive.{Ack, Observer, Observable}
+import monifu.reactive.internals._
+
+import scala.collection.mutable
+import scala.concurrent.Future
+import scala.concurrent.duration.FiniteDuration
+import scala.util.control.NonFatal
+
+
+object take {
+  /**
+   * Implementation for [[monifu.reactive.Observable.take]].
+   */
+  def left[T](source: Observable[T], n: Int): Observable[T] =
+    Observable.create { observer =>
+      source.unsafeSubscribe(new Observer[T] {
+        private[this] var counter = 0
+        private[this] var isDone = false
+
+        def onNext(elem: T) = {
+          if (n <= 0 && !isDone) {
+            isDone = true
+            observer.onComplete()
+            Cancel
+          }
+          else if (!isDone && counter < n) {
+            // ^^ short-circuit for not endlessly incrementing that number
+            counter += 1
+
+            if (counter < n) {
+              // this is not the last event in the stream, so send it directly
+              observer.onNext(elem)
+            }
+            else  {
+              // last event in the stream, so we need to send the event followed by an EOF downstream
+              // after which we signal upstream to the producer that it should stop
+              isDone = true
+              observer.onNext(elem)
+              observer.onComplete()
+              Cancel
+            }
+          }
+          else {
+            // we already emitted the maximum number of events, so signal upstream
+            // to the producer that it should stop sending events
+            Cancel
+          }
+        }
+
+        def onError(ex: Throwable) =
+          if (!isDone) {
+            isDone = true
+            observer.onError(ex)
+          }
+
+        def onComplete() =
+          if (!isDone) {
+            isDone = true
+            observer.onComplete()
+          }
+      })
+    }
+
+  /**
+   * Implementation for [[monifu.reactive.Observable.takeRight]].
+   */
+  def right[T](source: Observable[T], n: Int)(implicit s: Scheduler) =
+    Observable.create[T] { observer =>
+      source.unsafeSubscribe(new Observer[T] {
+        private[this] val queue = mutable.Queue.empty[T]
+        private[this] var queued = 0
+
+        def onNext(elem: T): Future[Ack] = {
+          if (n <= 0)
+            Cancel
+          else if (queued < n) {
+            queue.enqueue(elem)
+            queued += 1
+          }
+          else {
+            queue.enqueue(elem)
+            queue.dequeue()
+          }
+          Continue
+        }
+
+        def onError(ex: Throwable): Unit = {
+          queue.clear()
+          observer.onError(ex)
+        }
+
+        def onComplete(): Unit = {
+          Observable.from(queue).unsafeSubscribe(observer)
+        }
+      })
+    }
+
+  def leftByTimespan[T](source: Observable[T], timespan: FiniteDuration)(implicit s: Scheduler) =
+    Observable.create[T] { observer =>
+      source.unsafeSubscribe(new Observer[T] {
+        private[this] val lock = new AnyRef
+        private[this] var isDone = false
+
+        private[this] val task =
+          s.scheduleOnce(timespan, onComplete())
+
+        def onNext(elem: T): Future[Ack] =
+          lock.synchronized {
+            if (!isDone)
+              observer.onNext(elem).onCancel {
+                lock.synchronized {
+                  task.cancel()
+                  isDone = true
+                }
+              }
+            else
+              Cancel
+          }
+
+        def onError(ex: Throwable): Unit =
+          lock.synchronized {
+            if (!isDone) {
+              isDone = true
+              observer.onError(ex)
+            }
+          }
+
+        def onComplete(): Unit =
+          lock.synchronized {
+            if (!isDone) {
+              isDone = true
+              observer.onComplete()
+            }
+          }
+      })
+    }
+
+  /**
+   * Implementation for [[Observable.takeUntilOtherEmits]].
+   */
+  def untilOtherEmits[T, U](source: Observable[T], other: Observable[U])(implicit s: Scheduler) = {
+    Observable.create[T] { observer =>
+      // we've got contention between the source and the other observable
+      // and we can't use an Atomic here because of `onNext`, so we are
+      // forced to use a lock in order to preserve the non-concurrency
+      // clause in the contract
+      val lock = new AnyRef
+      // must be at all times synchronized by lock
+      var isDone = false
+
+      def terminate(ex: Throwable = null): Unit =
+        lock.synchronized {
+          if (!isDone) {
+            isDone = true
+            if (ex == null)
+              observer.onComplete()
+            else
+              observer.onError(ex)
+          }
+        }
+
+      source.unsafeSubscribe(new Observer[T] {
+        def onNext(elem: T) = lock.synchronized {
+          if (isDone) Cancel
+          else
+            observer.onNext(elem).onCancel {
+              lock.synchronized { isDone = true }
+            }
+        }
+
+        def onError(ex: Throwable): Unit = terminate(ex)
+        def onComplete(): Unit = terminate()
+      })
+
+      other.unsafeSubscribe(new SynchronousObserver[U] {
+        def onNext(elem: U) = {
+          terminate()
+          Cancel
+        }
+
+        def onError(ex: Throwable) = terminate(ex)
+        def onComplete() = terminate()
+      })
+    }
+  }
+
+  /**
+   * Implementation for [[monifu.reactive.Observable.takeWhile]].
+   */
+  def byPredicate[T](source: Observable[T])(p: T => Boolean) =
+    Observable.create[T] { observer =>
+      source.unsafeSubscribe(new Observer[T] {
+        var shouldContinue = true
+
+        def onNext(elem: T) = {
+          if (shouldContinue) {
+            // See Section 6.4. in the Rx Design Guidelines:
+            // Protect calls to user code from within an operator
+            var streamError = true
+            try {
+              val isValid = p(elem)
+              streamError = false
+              if (isValid) {
+                observer.onNext(elem)
+              }
+              else {
+                shouldContinue = false
+                observer.onComplete()
+                Cancel
+              }
+            }
+            catch {
+              case NonFatal(ex) =>
+                if (streamError) {
+                  observer.onError(ex)
+                  Cancel
+                }
+                else
+                  Future.failed(ex)
+            }
+          }
+          else
+            Cancel
+        }
+
+        def onComplete() = {
+          observer.onComplete()
+        }
+
+        def onError(ex: Throwable) = {
+          observer.onError(ex)
+        }
+      })
+    }
+
+  /**
+   * Implementation for [[monifu.reactive.Observable.takeWhileRefIsTrue]].
+   */
+  def whileRefIsTrue[T](source: Observable[T], ref: AtomicBoolean) =
+    Observable.create[T] { observer =>
+      source.unsafeSubscribe(new Observer[T] {
+        var shouldContinue = true
+
+        def onNext(elem: T) = {
+          if (shouldContinue) {
+            if (ref.get)
+              observer.onNext(elem)
+            else {
+              shouldContinue = false
+              observer.onComplete()
+              Cancel
+            }
+          }
+          else
+            Cancel
+        }
+
+        def onComplete() = {
+          observer.onComplete()
+        }
+
+        def onError(ex: Throwable) = {
+          observer.onError(ex)
+        }
+      })
+    }
+}
