3fbb10e76bf92b231476b63b61dd00f954fa2606
Alexandru Nedelcu <alex@eloquentix.com>
2014-10-09 14:17:37 +0300

    Add more tck tests

--- jvm/monifu-rx/src/test/scala/monifu/reactive/streamsTCK/PublisherFlatMapTest.scala
+++ jvm/monifu-rx/src/test/scala/monifu/reactive/streamsTCK/PublisherFlatMapTest.scala
@@ -17,25 +17,11 @@
  * limitations under the License.
  */
 
-package monifu.reactive.tck
+package monifu.reactive.streamsTCK
 
 import monifu.concurrent.Implicits.globalScheduler
 import monifu.reactive.Observable
-import org.reactivestreams.Publisher
-import org.reactivestreams.tck.{PublisherVerification, TestEnvironment}
-import org.scalatest.testng.TestNGSuiteLike
 
-class PublisherSimpleRepeatTest
-  extends PublisherVerification[Int](new TestEnvironment(1000), 1000) with TestNGSuiteLike {
-
-  def createPublisher(elements: Long): Publisher[Int] = {
-    if (elements == Long.MaxValue)
-      Observable.repeat(1).publisher
-    else
-      Observable.repeat(1).take(elements.toInt).publisher
-  }
-
-  def createErrorStatePublisher(): Publisher[Int] = {
-    Observable.error(new RuntimeException).publisher[Int]
-  }
-}
+class PublisherFlatMapTest extends PublisherVerification[Int]({
+  Observable.repeat(1).flatMap(x => Observable.unit(x))
+})
