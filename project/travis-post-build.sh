93facf35fabe1200d22727714ca40d199f1fb131
Jisoo Park <xxxyel@gmail.com>
2016-07-22 21:19:41 +0900

    Replace hardcoded main scala version with Travis config

--- project/travis-post-build.sh
+++ project/travis-post-build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 
 cd `dirname $0`/..
-MAIN_VERSION="2.11.8"
 
-if [ "$TRAVIS_SCALA_VERSION" = "$MAIN_VERSION" ]; then
+if [ -z "$MAIN_SCALA_VERSION" ]; then
+    >&2 echo "Environment MAIN_SCALA_VERSION is not set. Check .travis.yml."
+    exit 1
+fi
+
+if [ "$TRAVIS_SCALA_VERSION" = "$MAIN_SCALA_VERSION" ]; then
     echo "Uploading coverage for Scala $TRAVIS_SCALA_VERSION"
     exec sbt coverageAggregate coverageReport && codecov
 else
