ec3dea05b03627c218cdc4e85a8fbd1e5ac07727
Alexandru Nedelcu <noreply@alexn.org>
2016-10-19 09:16:51 +0200

    Update 2.12.0-RC2

--- project/release.sh
+++ project/release.sh
@@ -5,9 +5,9 @@ echo "RELEASE"
 sbt -mem 4096 release
 
 VERSION=`git tag | grep "^v2." | sort | tail -n 1`
-echo "Publishing for Scala 2.12.0-RC1"
+echo "Publishing for Scala 2.12.0-RC2"
 echo
 
 git checkout "$VERSION"
-sbt -mem 4096 ";++2.12.0-RC1;clean;coreJVM/publishSigned;coreJS/publishSigned;scalaz72JVM/publishSigned;scalaz72JS/publishSigned"
+sbt -mem 4096 ";++2.12.0-RC2;clean;coreJVM/publishSigned;coreJS/publishSigned;scalaz72JVM/publishSigned;scalaz72JS/publishSigned"
 git checkout master
