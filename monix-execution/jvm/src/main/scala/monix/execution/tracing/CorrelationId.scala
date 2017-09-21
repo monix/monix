38bc2b69f3ee8b8d8327d6f18132f1e18d430d66
Leandro Bolivar <leandrob131@gmail.com>
2017-09-21 17:34:16 -0500

    Made CorrelationId final and improved scaladoc

--- monix-execution/jvm/src/main/scala/monix/execution/tracing/CorrelationId.scala
+++ monix-execution/jvm/src/main/scala/monix/execution/tracing/CorrelationId.scala
@@ -3,11 +3,22 @@ package monix.execution.tracing
 import monix.execution.misc.{TracingContext, TracingContextCompanion}
 
 /**
-  * A tracing context that allows to propagate a correlation id
-  * through an execution.
+  * A [[CorrelationId]] is a [[monix.execution.misc.TracingContext TracingContext]] that allows
+  * to propagate a correlation id through an execution.
+  *
+  * {{{
+  *   import monix.execution.Scheduler.Implicits.traced
+  *
+  *   def getId: Task[Option[String]] = Task.pure(CorrelationId.current.map(_.id))
+  *
+  *   // Should yield Some(123456789)
+  *   val res = CorrelationId("123456789").asCurrent {
+  *     getId.runAsync
+  *   }
+  * }}}
   * @param id the correlation id.
   */
-case class CorrelationId(id: String) extends TracingContext {
+final case class CorrelationId(id: String) extends TracingContext {
 
   def asCurrent[T](f: => T): T = CorrelationId.let(Some(this))(f)
 }
