801cea39108318d6acca6428f08c6fd86d8afcba
Alexandru Nedelcu <noreply@alexn.org>
2017-12-23 19:48:21 +0200

    Refactor run-loop, add benchmarks

---
+++ benchmarks/shared/src/main/scala/monix/benchmarks/TaskDeepBindBenchmark.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2014-2017 by The Monix Project Developers.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monix.benchmarks
+
+import java.util.concurrent.TimeUnit
+import monix.eval.Task
+import org.openjdk.jmh.annotations._
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+/** To do comparative benchmarks between versions:
+  *
+  *     benchmarks/run-benchmark TaskDeepBindBenchmark
+  *
+  * This will generate results in `benchmarks/results`.
+  *
+  * Or to run the benchmark from within SBT:
+  *
+  *     jmh:run -i 10 -wi 10 -f 2 -t 1 monix.benchmarks.TaskDeepBindBenchmark
+  *
+  * Which means "10 iterations", "10 warm-up iterations", "2 forks", "1 thread".
+  * Please note that benchmarks should be usually executed at least in
+  * 10 iterations (as a rule of thumb), but more is better.
+  */
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+class TaskDeepBindBenchmark {
+  @Param(Array("3000"))
+  var size: Int = _
+
+  @Benchmark
+  def pure(): Int = {
+    def loop(i: Int): Task[Int] =
+      for {
+        j <- Task.pure(i)
+        _ <- if(j > size) Task.pure(j) else loop(j + 1)
+      } yield j
+
+    Await.result(loop(0).runAsync, Duration.Inf)
+  }
+
+  @Benchmark
+  def delay(): Int = {
+    def loop(i: Int): Task[Int] =
+      for {
+        j <- Task.eval(i)
+        _ <- if(j > size) Task.eval(j) else loop(j + 1)
+      } yield j
+
+    Await.result(loop(0).runAsync, Duration.Inf)
+  }
+
+  @Benchmark
+  def async(): Int = {
+    def loop(i: Int): Task[Int] =
+      for {
+        j <- Task(i)
+        _ <- if(j > size) Task.eval(j) else loop(j + 1)
+      } yield j
+
+    Await.result(loop(0).runAsync, Duration.Inf)
+  }
+}
---
+++ benchmarks/shared/src/main/scala/monix/benchmarks/TaskHandleErrorBenchmark.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2014-2017 by The Monix Project Developers.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monix.benchmarks
+
+import java.util.concurrent.TimeUnit
+import monix.eval.Task
+import org.openjdk.jmh.annotations._
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+/** To do comparative benchmarks between versions:
+  *
+  *     benchmarks/run-benchmark TaskHandleErrorBenchmark
+  *
+  * This will generate results in `benchmarks/results`.
+  *
+  * Or to run the benchmark from within SBT:
+  *
+  *     jmh:run -i 10 -wi 10 -f 2 -t 1 monix.benchmarks.TaskHandleErrorBenchmark
+  *
+  * Which means "10 iterations", "10 warm-up iterations", "2 forks", "1 thread".
+  * Please note that benchmarks should be usually executed at least in
+  * 10 iterations (as a rule of thumb), but more is better.
+  */
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+class TaskHandleErrorBenchmark {
+  @Param(Array("10000"))
+  var size: Int = _
+
+  @Benchmark
+  def happyPath(): Int = {
+    def loop(i: Int): Task[Int] =
+      if (i < size)
+        Task.pure(i + 1)
+          .onErrorHandleWith(Task.raiseError)
+          .flatMap(loop)
+      else
+        Task.pure(i)
+
+    Await.result(loop(0).runAsync, Duration.Inf)
+  }
+
+  @Benchmark
+  def errorRaised(): Int = {
+    val dummy = new RuntimeException("dummy")
+
+    def loop(i: Int): Task[Int] =
+      if (i < size)
+        Task.raiseError[Int](dummy)
+          .flatMap(x => Task.pure(x + 1))
+          .flatMap(x => Task.pure(x + 1))
+          .onErrorHandleWith(_ => loop(i + 1))
+      else
+        Task.pure(i)
+
+    Await.result(loop(0).runAsync, Duration.Inf)
+  }
+}
