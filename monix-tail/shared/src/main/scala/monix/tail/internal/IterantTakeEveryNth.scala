ffdb6f2311c7426e226afb798e6abca85a2c34c8
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-01-10 08:33:11 +0200

    Add `Iterant.takeEveryNth()` draft implementation;

---
+++ monix-tail/shared/src/main/scala/monix/tail/internal/IterantTakeEveryNth.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2014-2018 by The Monix Project Developers.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monix.tail.internal
+
+import monix.tail.Iterant.{Halt, Last, Next, NextBatch, NextCursor, Suspend}
+import monix.tail.Iterant
+import cats.syntax.all._
+import cats.effect.Sync
+import monix.execution.misc.NonFatal
+import monix.tail.batches.BatchCursor
+import monix.tail.internal.IterantUtils._
+
+import scala.collection.mutable.ArrayBuffer
+
+private[tail] object IterantTakeEveryNth {
+  /**
+    * Implementation for `Iterant#takeEveryNth`
+    */
+  def apply[F[_], A](source: Iterant[F, A], n: Int)
+                    (implicit F: Sync[F]): Iterant[F, A] = {
+
+    require(n > 0, "n must be strictly positive")
+
+    // TODO shortcut for n = 1
+
+    def processSeq(index: Int, ref: NextCursor[F, A]): NextCursor[F, A] = {
+      val NextCursor(cursor, rest, stop) = ref
+      val buffer = ArrayBuffer.empty[A]
+
+      var idx = index
+      var toProcess = cursor.recommendedBatchSize
+
+      // protects against infinite cursors
+      while (toProcess > 0 && cursor.hasNext()) {
+        if (idx == 1) {
+          buffer += cursor.next()
+          idx = n
+        } else {
+          cursor.next()
+          idx -= 1
+        }
+        toProcess -= 1
+      }
+
+      val next: F[Iterant[F, A]] = if (cursor.hasNext()) F.pure(ref) else rest
+      NextCursor(BatchCursor.fromAnyArray(buffer.toArray[Any]), next.map(loop(idx)), stop)
+    }
+
+
+    def loop(index: Int)(source: Iterant[F,A]): Iterant[F,A] = {
+      try source match {
+        case Next(item, rest, stop) =>
+          if (index == 1) Next(item, rest.map(loop(n)), stop)
+          else Suspend(rest.map(loop(index - 1)), stop)
+        case ref@NextCursor(_, _, _) =>
+          processSeq(index, ref)
+        case NextBatch(batch, rest, stop) =>
+          processSeq(index, NextCursor(batch.cursor(), rest, stop))
+        case Suspend(rest, stop) =>
+          Suspend(rest.map(loop(index)), stop)
+        case last @ Last(_) =>
+          if (index == 1) last else Halt(None)
+        case halt @ Halt(_) =>
+          halt
+      }
+      catch {
+        case ex if NonFatal(ex) => signalError(source, ex)
+      }
+    }
+
+    source match {
+      case NextBatch(_, _, _) | NextCursor(_, _, _) =>
+        // We can have side-effects with NextBatch/NextCursor
+        // processing, so suspending execution in this case
+        Suspend(F.delay(loop(n)(source)), source.earlyStop)
+      case _ =>
+        loop(n)(source)
+    }
+  }
+}
---
+++ monix-tail/shared/src/test/scala/monix/tail/IterantTakeEveryNthSuite.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2014-2018 by The Monix Project Developers.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monix.tail
+
+import cats.effect.Sync
+import cats.laws._
+import cats.laws.discipline._
+import monix.eval.{Coeval, Task}
+import monix.execution.internal.Platform
+import org.scalacheck.Test
+import org.scalacheck.Test.Parameters
+
+object IterantTakeEveryNthSuite extends BaseTestSuite {
+  override lazy val checkConfig: Parameters = {
+    if (Platform.isJVM)
+      Test.Parameters.default.withMaxSize(256)
+    else
+      Test.Parameters.default.withMaxSize(32)
+  }
+
+  def naiveImp[F[_], A](iter: Iterant[F, A], takeEveryNth: Int)(implicit F: Sync[F]): Iterant[F, A] =
+    iter
+      .zipWithIndex
+      .flatMap {
+        case (a, idx) =>
+          if ((idx + 1) % takeEveryNth == 0)
+            Iterant[F].pure(a)
+          else
+            Iterant[F].empty
+      }
+
+  test("naiveImp smoke test") { implicit s =>
+    val iter = Iterant[Coeval].fromList(List(1, 2, 3, 4, 5, 6))
+    assertEquals(naiveImp(iter, 1).toListL.value, List(1, 2, 3, 4, 5, 6))
+    assertEquals(naiveImp(iter, 2).toListL.value, List(2, 4, 6))
+    assertEquals(naiveImp(iter, 3).toListL.value, List(3, 6))
+    assertEquals(naiveImp(iter, 7).toListL.value, List.empty[Int])
+  }
+
+  test("Iterant[Task].takeEveryNth equivalence with naiveImp") { implicit s =>
+    check3 { (list: List[Int], idx: Int, nr: Int) =>
+      val stream = arbitraryListToIterant[Task, Int](list, math.abs(idx) + 1, allowErrors = false)
+      val length = list.length
+      // TODO cover from 1 up to length + 1
+      val n = math.max(length / 3, 1)
+
+      val res = stream.takeEveryNth(n).toListL.runAsync
+      val exp = naiveImp(stream, n).toListL.runAsync
+      s.tick()
+      res.value <-> exp.value
+    }
+  }
+
+  // TODO cover various scenarios (early stop, broken batches, etc.)
+
+}
