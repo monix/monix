76a66b49b39507ff15e2ad4fb9ffdc66c2078080
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2018-01-15 21:09:29 +0300

    add automatic header creation on compilation (#531)

--- monix-execution/jvm/src/main/scala_2.12/monix/execution/internal/forkJoin/package.scala
+++ monix-execution/jvm/src/main/scala_2.12/monix/execution/internal/forkJoin/package.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2017 by its authors. Some rights reserved.
+ * Copyright (c) 2014-2018 by The Monix Project Developers.
  * See the project homepage at: https://monix.io
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
