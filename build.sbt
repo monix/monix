c6a2452686957ef26f4cbe054df8162a1d50c9b4
Alexandru Nedelcu <noreply@alexn.org>
2018-01-22 03:17:44 +0200

    Deactivate the noSources setting

--- build.sbt
+++ build.sbt
@@ -24,13 +24,6 @@ lazy val doNotPublishArtifact = Seq(
   publishArtifact in (Compile, packageBin) := false
 )
 
-lazy val noSources = Seq(
-  // disable automatic dependency on the Scala library
-  autoScalaLibrary := false,
-  publishArtifact in Compile := false,
-  sources in Compile := Seq.empty
-)
-
 lazy val warnUnusedImport = Seq(
   scalacOptions ++= {
     CrossVersion.partialVersion(scalaVersion.value) match {
@@ -311,7 +304,6 @@ lazy val coreJVM = project.in(file("monix/jvm"))
   .dependsOn(typesJVM, executionJVM, evalJVM, reactiveJVM)
   .aggregate(typesJVM, executionJVM, evalJVM, reactiveJVM, catsJVM, scalaz72JVM)
   .settings(crossSettings)
-  .settings(noSources)
   .settings(name := "monix")
 
 lazy val coreJS = project.in(file("monix/js"))
---
+++ monix/shared/src/main/scala/monix/package.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2014-2018 by The Monix Project Developers.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** Base package for the Monix library.
+  *
+  * The core is being split in the following sub-packages:
+  *
+  *  - '''[[monix.execution]]''' exposes lower level primitives for dealing
+  *    with asynchronous execution, corresponding to the `monix-execution`
+  *    sub-project
+  *  - '''[[monix.eval]]''' is for dealing with evaluation of results,
+  *    thus exposing [[monix.eval.Task Task]] and [[monix.eval.Coeval Coeval]],
+  *    corresponding to the `monix-eval` sub-project
+  *  - '''[[monix.reactive]]''' exposes the [[monix.reactive.Observable Observable]]
+  *    pattern, corresponding to the `monix-reactive` sub-project
+  */
+package object monix {}
