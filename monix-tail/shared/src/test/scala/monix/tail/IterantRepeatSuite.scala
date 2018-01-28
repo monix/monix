d353a632d19a31a84a5214dc96f36918acbc8158
Avasil <pgawrys2@gmail.com>
2018-01-28 10:29:21 +0100

    added missing tests

--- monix-tail/shared/src/test/scala/monix/tail/IterantRepeatSuite.scala
+++ monix-tail/shared/src/test/scala/monix/tail/IterantRepeatSuite.scala
@@ -17,7 +17,10 @@
 
 package monix.tail
 
-import monix.eval.Coeval
+import cats.laws._
+import cats.laws.discipline._
+import monix.eval.{Coeval, Task}
+import monix.execution.cancelables.BooleanCancelable
 import monix.execution.exceptions.DummyException
 import monix.tail.batches.{Batch, BatchCursor}
 
@@ -35,6 +38,47 @@ object IterantRepeatSuite extends BaseTestSuite {
       }}.toListL.value}
 
     assertEquals(values, expectedValues)
+  }
+
+  test("Iterant.repeat protects against broken batches") { implicit s =>
+    check1 { (iter: Iterant[Task, Int]) =>
+      val dummy = DummyException("dummy")
+      val suffix = Iterant[Task].nextBatchS[Int](new ThrowExceptionBatch(dummy), Task.now(Iterant[Task].empty), Task.unit)
+      val stream = iter.onErrorIgnore ++ suffix
+      val received = stream.repeat
+      received <-> Iterant[Task].haltS[Int](Some(dummy))
+    }
+  }
+
+  test("Iterant.repeat protects against broken cursors") { implicit s =>
+    check1 { (iter: Iterant[Task, Int]) =>
+      val dummy = DummyException("dummy")
+      val suffix = Iterant[Task].nextCursorS[Int](new ThrowExceptionCursor(dummy), Task.now(Iterant[Task].empty), Task.unit)
+      val stream = iter.onErrorIgnore ++ suffix
+      val received = stream.repeat
+      received <-> Iterant[Task].haltS[Int](Some(dummy))
+    }
+  }
+
+  test("Iterant.repeat preserves the source earlyStop") { implicit s =>
+    var effect = 0
+    val stop = Coeval.eval(effect += 1)
+    val source = Iterant[Coeval].nextCursorS(BatchCursor(1,2,3), Coeval.now(Iterant[Coeval].empty[Int]), stop)
+    val stream = source.repeat
+    stream.earlyStop.value
+    assertEquals(effect, 1)
+  }
+
+  test("Iterant.repeat triggers early stop on exception") { _ =>
+    check1 { (iter: Iterant[Coeval, Int]) =>
+      val cancelable = BooleanCancelable()
+      val dummy = DummyException("dummy")
+      val suffix = Iterant[Coeval].nextCursorS[Int](new ThrowExceptionCursor(dummy), Coeval.now(Iterant[Coeval].empty), Coeval.unit)
+      val stream = (iter.onErrorIgnore ++ suffix).doOnEarlyStop(Coeval.eval(cancelable.cancel()))
+
+      intercept[DummyException] { stream.repeat.toListL.value }
+      cancelable.isCanceled
+    }
   }
 
   test("Iterant.repeat works for NextBatch") { implicit s =>
@@ -73,6 +117,21 @@ object IterantRepeatSuite extends BaseTestSuite {
     val expectedValues = List.fill(6)(1)
     val dummy = DummyException("dummy")
     val source = Iterant[Coeval].lastS(1)
+
+    intercept[DummyException] {
+      source.repeat.map { x => if (effect == 6) throw dummy else {
+        effect += 1; values ::= x; x
+      }}.toListL.value}
+
+    assertEquals(values, expectedValues)
+  }
+
+  test("Iterant.repeat works for Suspend") { implicit s =>
+    var effect = 0
+    var values = List[Int]()
+    val expectedValues = List.fill(6)(1)
+    val dummy = DummyException("dummy")
+    val source = Iterant[Coeval].suspendS[Int](Coeval(Iterant.nextS(1, Coeval(Iterant.empty), Coeval.unit)), Coeval.unit)
 
     intercept[DummyException] {
       source.repeat.map { x => if (effect == 6) throw dummy else {
@@ -121,6 +180,6 @@ object IterantRepeatSuite extends BaseTestSuite {
   test("Iterant.repeat builder terminates if the source is empty") { implicit s =>
     val source = Iterant[Coeval].empty[Int]
 
-    assertEquals(source.repeat, source)
+    assertEquals(Iterant[Coeval].repeat(Seq(): _*), source)
   }
 }
