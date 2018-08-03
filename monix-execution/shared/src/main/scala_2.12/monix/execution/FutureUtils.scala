2c9a5e33ab6bab82150d6b32df4f176843343124
LMnet <lmnet89@gmail.com>
2018-08-03 11:59:32 +0700

    Fix unwanted side effect in FutureUtils.timeoutTo (#695)

--- monix-execution/shared/src/main/scala_2.12/monix/execution/FutureUtils.scala
+++ monix-execution/shared/src/main/scala_2.12/monix/execution/FutureUtils.scala
@@ -66,18 +66,24 @@ object FutureUtils {
   def timeoutTo[A](source: Future[A], atMost: FiniteDuration, fallback: => Future[A])
     (implicit s: Scheduler): Future[A] = {
 
-    val promise = Promise[A]()
+    val promise = Promise[Either[Unit, Try[A]]]()
     val task = s.scheduleOnce(atMost.length, atMost.unit,
-      new Runnable { def run() = promise.tryCompleteWith(fallback) })
+      new Runnable { def run() = promise.trySuccess(Left(())) })
 
     source.onComplete { r =>
       // canceling task to prevent waisted CPU resources and memory leaks
       // if the task has been executed already, this has no effect
       task.cancel()
-      promise.tryComplete(r)
+      promise.trySuccess(Right(r))
     }
 
-    promise.future
+    promise.future.flatMap {
+      case Right(res) => Future.fromTry(res)
+      case Left(_) =>
+        // evaluate fallback only here to exclude possibility of race condition
+        // between source and fallback when they are finishing at the same time
+        fallback
+    }
   }
 
   /** Utility that lifts a `Future[A]` into a `Future[Try[A]]`, exposing
