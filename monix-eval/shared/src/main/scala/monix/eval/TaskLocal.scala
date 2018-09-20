4d4dcf5ec43c1933449c3f9a8d7953d0b3548b88
Alexandru Nedelcu <noreply@alexn.org>
2018-09-20 11:12:20 +0300

    TaskLocal refactorings (final, hopefully)

--- monix-eval/shared/src/main/scala/monix/eval/TaskLocal.scala
+++ monix-eval/shared/src/main/scala/monix/eval/TaskLocal.scala
@@ -20,7 +20,8 @@ package monix.eval
 import cats.effect.Resource
 import monix.eval.Task.ContextSwitch
 import monix.eval.internal.TaskContext
-import monix.execution.annotations.UnsafeBecauseImpure
+import monix.execution.annotations.{UnsafeBecauseImpure, UnsafeProtocol}
+import monix.execution.exceptions.APIContractViolationException
 import monix.execution.misc.Local
 
 /** A `TaskLocal` is like a
@@ -69,31 +70,30 @@ import monix.execution.misc.Local
   *   val result = task.runAsync
   * }}}
   */
-final class TaskLocal[A] private (
-  ref: Local[A],
-  ctx: TaskLocal.Context,
-  guard: () => Unit) {
+final class TaskLocal[A] private (ref: Local[A]) {
+  import TaskLocal.checkPropagation
 
-  /** Returns [[monix.execution.misc.Local]] instance used in this [[TaskLocal]].
+  /**
+    * Returns [[monix.execution.misc.Local]] instance used in this [[TaskLocal]].
     *
     * Note that `TaskLocal.bind` will restore the original local value
     * on the thread where the `Task's` run-loop ends up so it might lead
     * to leaving local modified in other thread.
     */
   def local: Task[Local[A]] =
-    Task.eval { guard(); ref }
+    Task(ref)
 
   /** Returns the current local value (in the `Task` context). */
   def read: Task[A] =
-    Task.eval { guard(); ref.get }
+    checkPropagation(Task(ref.get))
 
   /** Updates the local value. */
   def write(value: A): Task[Unit] =
-    Task.eval { guard(); ref.update(value) }
+    checkPropagation(Task(ref.update(value)))
 
   /** Clears the local value, making it return its `default`. */
   def clear: Task[Unit] =
-    Task.eval { guard(); ref.clear() }
+    checkPropagation(Task(ref.clear()))
 
   /** Binds the local var to a `value` for the duration of the given
     * `task` execution.
@@ -180,31 +180,29 @@ final class TaskLocal[A] private (
     }
 
   private def restore(value: Option[A]): Task[Unit] =
-    Task.eval { guard(); ref.value = value }
+    Task(ref.value = value)
 }
 
 /**
   * Builders for [[TaskLocal]]
   *
-  * @define resourceDesc The returned value is a Cats-Effect `Resource`
-  *         because `TaskLocal` values can only be managed via an active
-  *         [[TaskLocal.Context]] that ensures the underlying run-loop can
-  *         manage locals (transport them over async boundaries via
-  *         `Task`'s run-loop implementation). The usage of `Resource`
-  *         ensures that the underlying engine switches ON the mode
-  *         for working with locals, switching it off when it's done.
+  * @define resourceDesc The returned value is a Cats-Effect
+  *         `Resource` because `TaskLocal` values can only be managed
+  *         with an active context that ensures the underlying
+  *         run-loop can manage locals (transport them over async
+  *         boundaries via `Task`'s run-loop implementation). The
+  *         usage of `Resource` ensures that the underlying engine
+  *         switches ON the mode for working with locals, switching it
+  *         OFF when it's done.
   *
   *         The Cats-Effect `Resource` data type is a pure and safe
   *         way to manage resources. See
   *         [[https://typelevel.org/cats-effect/datatypes/resource.html the documentation]].
   *
-  * @define contextNote NOTE: when using more than one `TaskLocal` value it
-  *     might be better to use the builders exposed by [[TaskLocal.context]]
-  *     instead.
-  *
-  *     WARN: all usage happens inside the context of the given `Resource`,
-  *     via `use`. If a `TaskLocal` value leaks outside of this context,
-  *     its usage will start throwing `IllegalStateException` errors.
+  * @define contextNote WARN: all usage happens inside the context of
+  *         the given `Resource`, via `use`. If a `TaskLocal` value
+  *         leaks outside of this context, its usage will start
+  *         throwing `IllegalStateException` errors.
   */
 object TaskLocal {
   /**
@@ -238,14 +236,12 @@ object TaskLocal {
     *
     * $contextNote
     *
-    * @see [[Context.create]]
-    *
     * @param default is a value that gets returned in case the
     *        local was never updated (with [[TaskLocal.write write]])
     *        or in case it was cleared (with [[TaskLocal.clear]])
     */
   def create[A](default: A): Resource[Task, TaskLocal[A]] =
-    context.flatMap(locals => Resource.pure(locals.unsafe(default)))
+    createResource(Task(Unsafe.create(default)))
 
   /** Builds a [[TaskLocal]] reference with the given `default`,
     * being lazily evaluated, using [[Coeval]] to manage evaluation
@@ -258,15 +254,13 @@ object TaskLocal {
     *
     * $contextNote
     *
-    * @see [[Context.createF]]
-    *
     * @param default is a value that gets returned in case the
     *        local was never updated (with [[TaskLocal.write write]])
     *        or in case it was cleared (with [[TaskLocal.clear]]),
     *        lazily evaluated and managed by [[Coeval]] (via [[CoevalLike]])
     */
   def createF[F[_], A](default: F[A])(implicit F: CoevalLike[F]): Resource[Task, TaskLocal[A]] =
-    context.flatMap(locals => Resource.pure(locals.unsafeF(default)))
+    createResource(Task(Unsafe.createF(default)))
 
   /**
     * Wraps an unsafe [[monix.execution.misc.Local]] into a [[TaskLocal]].
@@ -275,189 +269,163 @@ object TaskLocal {
     *
     * $contextNote
     *
-    * @see [[Context.wrap]]
-    *
     * @param local is a [[monix.execution.misc.Local Local]] value evaluated
     *        lazily with any effect data type that is [[TaskLike]]
     */
   def wrap[F[_], A](local: F[Local[A]])(implicit F: TaskLike[F]): Resource[Task, TaskLocal[A]] =
-    context.flatMap(locals => Resource.liftF(locals.wrap(local)))
+    createResource(F.toTask(local).map(Unsafe.wrap))
 
   /**
-    * Creates a new `TaskLocal` context, managed via Cats-Effect's `Resource`.
+    * Unsafe builders and helpers for dealing with [[TaskLocal]].
     *
-    * $resourceDesc
+    * A `TaskLocal` value should be considered a "resource" with a scope
+    * in which it can be used, because internally locals need support
+    * from Task's run-loop, support that needs to be turned ON and OFF.
+    * Which is why for normal usage users are encouraged to use the
+    * normal builders making use of Cats-Effect's `Resource`.
     *
-    * Example:
+    * But if you know what you're doing [[TaskLocal.Unsafe]] exposes
+    * unsafe helpers and builders for dealing with `TaskLocal`, for when
+    * you know what you're doing.
     *
-    * {{{
-    *   import monix.eval.{Task, TaskLocal}
+    * The unsafe builders like [[Unsafe.create]], [[Unsafe.createF]] and
+    * [[Unsafe.wrap]] are unsafe not only due to the protocol, but also
+    * because they break referential transparency. So use with care.
     *
-    *   // Here `use` will open a new `Context`
-    *   TaskLocal.context.use { locals =>
-    *     for {
-    *       // The builder is invoked via the locals context:
-    *       local <- locals.create(0)
-    *       value1 <- local.read // value1 == 0
-    *       _ <- local.write(100)
-    *       value2 <- local.read // value2 == 100
-    *       value3 <- local.bind(200)(local.read.map(_ * 2)) // value3 == 200 * 2
-    *       value4 <- local.read // value4 == 100
-    *       _ <- local.clear
-    *       value5 <- local.read // value5 == 0
-    *     } yield {
-    *       // Should print 0, 100, 400, 100, 0
-    *       println("value1: " + value1)
-    *       println("value2: " + value2)
-    *       println("value3: " + value3)
-    *       println("value4: " + value4)
-    *       println("value5: " + value5)
-    *     }
-    *   }
-    * }}}
-    *
-    * WARNING: if locals leak the context of the provided `Resource`, usage
-    * is illegal and they start throwing `IllegalStateException`.
-    *
-    * As example this will throw an exception:
-    *
-    * {{{
-    *   // BROKEN sample!
-    *   for {
-    *     // This leaks the local reference
-    *     local <- TaskLocal.context.use(_.create(0))
-    *     // Will throw error because no longer in a locals context!
-    *     _     <- local.get
-    *   } yield ()
-    * }}}
-    */
-  def context: Resource[Task, Context] =
-    resourceRef
-
-  /**
-    * The [[TaskLocal.Context]] is a resource that can be used for creating
-    * and using locals.
-    *
-    * In usage of [[Task]] the runtime needs to cooperate with `TaskLocal`
-    * values, to safely transport them over asynchronous boundaries. In order
-    * to turn ON this support, usage of locals needs to be within the scope
-    * of a locals context, i.e. with an active [[TaskLocal.Context]].
-    *
-    * Therefore a `TaskLocal.Context` is a resource that can be used to
-    * create `TaskLocal` values and:
-    *
-    *   1. once created it activates `Task`'s underlying support for locals
-    *   1. once closed it deactivates that support and locals, should they leak
-    *      outside the context of their `Resource` will start throwing exceptions
-    *
-    * @see [[TaskLocal.context]] for creating such a context.
-    *
-    * @define refTransparency When creating a `TaskLocal` an underlying
-    *         variable gets allocated and this in itself breaks referential
-    *         transparency. This builder is "unsafe" because it breaks
-    *         referential transparency.
+    * @define refTransparency WARNING (referential transparency): When creating
+    *         a `TaskLocal` an underlying variable gets allocated and this in
+    *         itself breaks referential transparency. This builder is "unsafe"
+    *         because it breaks referential transparency.
     *
     *         Only use if you know what you're doing, but use the "safe"
     *         builders if you want to ensure you're doing functional programming,
     *         with all the benefits that brings.
+    *
+    * @define unsafeProtocol WARNING (protocol): The protocol for using this
+    *         is considered unsafe because a proper locals context is not enforced.
+    *
+    *         Normally the underlying support for usage of `TaskLocal` needs to
+    *         be turned ON and OFF in the `Task` implementation and usage of
+    *         `Resource` values in the API helps with doing static type checks
+    *         and to ensure that usage is correct. But in instances
+    *         in which the user knows what he's doing, we can side step that
+    *         and simply use locals in combination with [[Unsafe.scopedTask]]
+    *         or [[Unsafe.scopedResource]].
     */
-  final class Context private[TaskLocal] (isActive: Coeval[Boolean]) {
-    /** Builds a [[TaskLocal]] reference with the given default in the
-      * current context.
+  @UnsafeProtocol object Unsafe {
+    /**
+      * Ensures that the underlying support for `TaskLocal` is enabled when
+      * evaluating the given `task`.
       *
-      * @param default is a value that gets returned in case the
-      *        local was never updated (with [[TaskLocal.write write]])
-      *        or in case it was cleared (with [[TaskLocal.clear]])
+      * Example:
+      *
+      * {{{
+      *   {
+      *     TaskLocal.Unsafe.scopedTask {
+      *       for {
+      *         local <- Task(TaskLocal.Unsafe.create(0))
+      *         _     <- local.write(10)
+      *         _     <- Task.shift
+      *         value <- local.read
+      *       } yield value
+      *     }
+      *   }
+      * }}}
+      *
+      * In this example all operations done on `TaskLocal` are enclosed in
+      * a `scopedTask` call, so this ensures that the underlying support
+      * for locals is enabled when evaluating the given task.
+      *
+      * $unsafeProtocol
       */
-    def create[A](default: A): Task[TaskLocal[A]] =
-      Task.eval(unsafe(default))
+    def scopedTask[A](task: Task[A]): Task[A] =
+      ContextSwitch(task, enablePropagation, disablePropagation)
 
-    /** Builds a [[TaskLocal]] reference with the given `default`,
-      * being lazily evaluated, using [[Coeval]] to manage evaluation
-      * (or compatible data types, see [[CoevalLike]]).
+    /** Ensures that the underlying support for `TaskLocal` is enabled when
+      * evaluating the given resource.
       *
-      * Side effects in `default` are allowed, [[Coeval]] being a data type
-      * that's safe for side effects.
+      * Example:
       *
-      * @param default is a value that gets returned in case the
-      *        local was never updated (with [[TaskLocal.write write]])
-      *        or in case it was cleared (with [[TaskLocal.clear]]),
-      *        lazily evaluated and managed by [[Coeval]] (via [[CoevalLike]])
+      * {{{
+      *   {
+      *     import Task.Unsafe.canUseLocals
+      *
+      *     TaskLocal.Unsafe.scopedResource {
+      *       for {
+      *         local1 <- Resource.liftF(Task(TaskLocal.Unsafe.create(0)))
+      *         local2 <- Resource.liftF(Task(TaskLocal.Unsafe.create(0)))
+      *       } yield (local1, local2)
+      *     }
+      *   }
+      * }}}
+      *
+      * In this example the created `Resource` value will ensure that the
+      * underlying support for locals is enabled for the entire lifecycle
+      * of the created resource.
+      *
+      * $unsafeProtocol
       */
-    def createF[F[_], A](default: F[A])(implicit F: CoevalLike[F]): Task[TaskLocal[A]] =
-      Task.eval(unsafeF(default))
-
-    /** Wraps an unsafe [[monix.execution.misc.Local]] into a [[TaskLocal]].
-      *
-      * @param local is a [[monix.execution.misc.Local Local]] value evaluated
-      *        lazily with any effect data type that is [[TaskLike]]
-      */
-    def wrap[F[_], A](local: F[Local[A]])(implicit F: TaskLike[F]): Task[TaskLocal[A]] =
-      F.toTask(local).map(unsafeWrap)
+    def scopedResource[A](res: Resource[Task, A]): Resource[Task, A] =
+      createResource(Task.unit).flatMap(_ => res)
 
     /** Unsafe builder for [[TaskLocal]] that initializes the value with
       * the given strictly evaluated `default`.
       *
+      * $unsafeProtocol
+      *
       * $refTransparency
       *
-      * @see [[create]] for the safe version
+      * @see [[TaskLocal.create]] for the safe version
       */
+    @UnsafeProtocol
     @UnsafeBecauseImpure
-    def unsafe[A](default: A): TaskLocal[A] = {
-      if (isActive())
-        new TaskLocal(new Local[A](default), this, guard)
-      else
-        reject()
+    def create[A](default: A): TaskLocal[A] = {
+      new TaskLocal(new Local[A](default))
     }
 
     /** Unsafe builder for [[TaskLocal]] that initializes the value with
       * the given lazily evaluated `default`.
       *
+      * $unsafeProtocol
+      *
       * $refTransparency
       *
-      * @see [[createF]] for the safe version
+      * @see [[TaskLocal.createF]] for the safe version
       */
+    @UnsafeProtocol
     @UnsafeBecauseImpure
-    def unsafeF[F[_], A](default: F[A])(implicit F: CoevalLike[F]): TaskLocal[A] = {
-      if (isActive()) {
-        val fa = F.toCoeval(default)
-        new TaskLocal[A](new Local(fa.value()), this, guard)
-      } else {
-        reject()
-      }
+    def createF[F[_], A](default: F[A])(implicit F: CoevalLike[F]): TaskLocal[A] = {
+      val fa = F.toCoeval(default)
+      new TaskLocal[A](new Local(fa.value()))
     }
 
     /** Unsafe builder for [[TaskLocal]] that wraps the given
       * [[monix.execution.misc.Local]].
       *
+      * $unsafeProtocol
+      *
       * $refTransparency
       *
-      * @see [[wrap]] for the safe version
+      * @see [[TaskLocal.wrap]] for the safe version
       */
+    @UnsafeProtocol
     @UnsafeBecauseImpure
-    def unsafeWrap[A](local: Local[A]): TaskLocal[A] = {
-      if (isActive())
-        new TaskLocal[A](local, this, guard)
-      else
-        reject()
+    def wrap[A](local: Local[A]): TaskLocal[A] = {
+      new TaskLocal[A](local)
+    }
+  }
+
+  private[this] val disablePropagation: (Any, Throwable, TaskContext, TaskContext) => TaskContext =
+    (_, _, old, current) => {
+      val opts = current.options.copy(localContextPropagation = old.options.localContextPropagation)
+      current.withOptions(opts)
     }
 
-    private[this] val guard: () => Unit =
-      () => { if (!isActive()) reject() }
-  }
-
-  private def reject(): Nothing = {
-    throw new IllegalStateException("TaskLocal.Context is no longer available")
-  }
-
-  private[this] def disablePropagation(cancel: Task[Unit]): Task[Unit] =
+  private[this] val disablePropagationTask: Task[Unit] =
     ContextSwitch(
-      cancel,
+      Task.unit,
       ctx => ctx,
-      (_, _, _, current) => {
-        val opts = current.options.copy(localContextPropagation = false)
-        current.withOptions(opts)
-      })
+      disablePropagation)
 
   private[this] val enablePropagation: TaskContext => TaskContext =
     ctx => {
@@ -473,16 +441,25 @@ object TaskLocal {
       trampolineBefore = false,
       trampolineAfter = true)
 
-  private[this] val resourceRef = Resource[Task, Context] {
+  private def createResource[A](tla: Task[A]) = Resource[Task, A] {
     readOption.flatMap { hasLocalPropagation =>
-      var isActive = true
-      val ctx = new Context(Coeval(isActive))
-      val cancel = Task { isActive = false }
-
       if (hasLocalPropagation)
-        Task.now((ctx, cancel))
+        tla.map(a => (a, Task.unit))
       else
-        ContextSwitch(Task((ctx, disablePropagation(cancel))), enablePropagation, null)
+        ContextSwitch(tla.map((_, disablePropagationTask)), enablePropagation, null)
     }
   }
+
+  private def checkPropagation[A](fa: Task[A]): Task[A] =
+    ContextSwitch(fa, checkPropagationRef, null)
+
+  private[this] val checkPropagationRef: TaskContext => TaskContext =
+    ctx => {
+      if (!ctx.options.localContextPropagation) {
+        throw new APIContractViolationException(
+          "TaskLocal not used in the context of a TaskLocal.scope! " +
+          "See documentation at: https://monix.io/api/current/monix/eval/TaskLocal.html")
+      }
+      ctx
+    }
 }
