22454578978593697fe0173484f9c1876869170b
Alexandru Nedelcu <noreply@alexn.org>
2018-11-06 12:02:03 +0200

    Add monix-catnap mention

--- monix/shared/src/main/scala/monix/package.scala
+++ monix/shared/src/main/scala/monix/package.scala
@@ -22,6 +22,9 @@
   *  - '''[[monix.execution]]''' exposes lower level primitives for dealing
   *    with asynchronous execution, corresponding to the `monix-execution`
   *    sub-project
+  *  - '''[[monix.catnap]]''' exposes pure abstractions built on top of
+  *    the [[https://typelevel.org/cats-effect/ Cats-Effect]] type classes,
+  *    corresponding to the `monix-catnap` sub-project
   *  - '''[[monix.eval]]''' is for dealing with evaluation of results,
   *    thus exposing [[monix.eval.Task Task]] and [[monix.eval.Coeval Coeval]],
   *    corresponding to the `monix-eval` sub-project
