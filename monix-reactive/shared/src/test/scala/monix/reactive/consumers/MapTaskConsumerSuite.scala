3c806cf196165a0dd39ff1a1fc2ca681f39f022e
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2019-07-10 22:16:42 +0300

    scalafmt, the bane of my existence

--- monix-reactive/shared/src/test/scala/monix/reactive/consumers/MapTaskConsumerSuite.scala
+++ monix-reactive/shared/src/test/scala/monix/reactive/consumers/MapTaskConsumerSuite.scala
@@ -24,7 +24,6 @@ import monix.execution.exceptions.DummyException
 import monix.reactive.{BaseTestSuite, Consumer, Observable}
 import scala.util.Failure
 
-
 object MapTaskConsumerSuite extends BaseTestSuite {
   test("consumer.mapTask equivalence with task.map") { implicit s =>
     check1 { (obs: Observable[Int]) =>
