46d91c2eac448b1b2bd0d4f90cecdccd7bd50ae7
Kasper Kondzielski <ghostbuster91@users.noreply.github.com>
2020-10-02 19:43:46 +0200

    Add support for compression: Gzip and deflate (#1244)
    * Add support for compression: Gzip and deflate
    
    * Implement manually inputstream.readAllBytes as it wasn't present in jdk8
    
    * [Refactor] CompressionLevel - change jValue to value
    
    * [Refactor] Mark CompressionParameters as final
    
    * [Refactor] DeflateOperator - remove redundant flag isDone
    
    * [Refactor] DeflateOperator - use continue as a default value for ack
    
    * [Refactor] Expose compression utils as stream transformation rather than operators
    
    * [Refactor] Use <-> to indicate function equality
    
    * [Refactor] Extract common part of GunzipTest and InflateTest
    
    * [Refactor] Compression operators should operate on array[Byte] rather than byte
    
    * [Compression] Fix GunzipOperator monix.Observable protocol violations
    
    * [Compression] Remove redundant flag from GzipOperator
    
    * [Compression] Fix GzipOperator monix.Observable protocol violations
    
    * [Compression] Fix DeflateOperator monix.Observable protocol violations
    
    * [Compression] Fix InflateOperator monix.Observable protocol violations
    
    * [Compression] Rename package (compress -> compression) to be consistent with fs2/zio
    
    * [Compression] Fix compilation on older versions of scala
    
    * [Compression] Refine tests by implementing BaseOperatorSuite and renaming them to follow convention
    
    * [Compression] Implement brokenUserCodeObservable test case for decompression tests
    
    * [Compression] Use provided error rather than hardcoded one
    
    * [Compression] Fix gunzipOperatorSuite and remove resolved todo

---
+++ monix-reactive/jvm/src/test/scala/monix/reactive/compression/GunzipOperatorSuite.scala
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2014-2020 by The Monix Project Developers.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monix.reactive.compression
+
+import minitest.api.AssertionException
+import monix.reactive.Observable
+
+import scala.concurrent.duration.Duration.Zero
+
+object GunzipOperatorSuite extends BaseDecompressionSuite with GzipTestsUtils {
+
+  testAsync("long input, no SYNC_FLUSH") { _ =>
+    jdkGzippedStream(longText, syncFlush = false)
+      .transform(gunzip(64))
+      .toListL
+      .map(list => assertEquals(list.flatten, longText.toList))
+      .runToFuture
+  }
+
+  testAsync("no output on very incomplete stream is not OK") { _ =>
+    Observable
+      .now((1 to 5).map(_.toByte).toArray)
+      .transform(gunzip())
+      .toListL
+      .map(_ => fail("should have failed"))
+      .onErrorRecover { case e if !e.isInstanceOf[AssertionException] => () }
+      .runToFuture
+  }
+  testAsync("parses header with FEXTRA") { _ =>
+    headerWithExtra
+      .transform(gunzip(64))
+      .toListL
+      .map(list => assertEquals(list.flatten, shortText.toList))
+      .runToFuture
+  }
+  testAsync("parses header with FCOMMENT") { _ =>
+    headerWithComment
+      .transform(gunzip(64))
+      .toListL
+      .map(list => assertEquals(list.flatten, shortText.toList))
+      .runToFuture
+  }
+  testAsync("parses header with FNAME") { _ =>
+    headerWithFileName
+      .transform(gunzip(64))
+      .toListL
+      .map(list => assertEquals(list.flatten, shortText.toList))
+      .runToFuture
+  }
+  testAsync("parses header with CRC16") { _ =>
+    headerWithCrc
+      .transform(gunzip(64))
+      .toListL
+      .map(list => assertEquals(list.flatten, shortText.toList))
+      .runToFuture
+  }
+  testAsync("parses header with CRC16, FNAME, FCOMMENT, FEXTRA") { _ =>
+    headerWithAll
+      .transform(gunzip(64))
+      .toListL
+      .map(list => assertEquals(list.flatten, shortText.toList))
+      .runToFuture
+  }
+
+  override def jdkCompressedStream(input: Array[Byte], chunkSize: Int): Observable[Array[Byte]] =
+    jdkGzippedStream(input, chunkSize = chunkSize)
+
+  override def decompress(bufferSize: Int): Observable[Array[Byte]] => Observable[Array[Byte]] = gunzip(bufferSize)
+
+  override def createObservable(sourceCount: Int): Option[GunzipOperatorSuite.Sample] =
+    Some {
+      val o = Observable
+        .repeatEval(jdkGzip(longText, syncFlush = false))
+        .take(sourceCount.toLong - 1)
+        .transform(gunzip())
+        .map(_ => 1L)
+      Sample(o, sourceCount, sourceCount, Zero, Zero)
+    }
+
+  override def brokenUserCodeObservable(sourceCount: Int, ex: Throwable): Option[GunzipOperatorSuite.Sample] =
+    Some {
+      val o = (Observable
+        .repeatEval(jdkGzip(longText, syncFlush = false))
+        .take(sourceCount.toLong)
+        .transform(gunzip()) ++ Observable
+        .repeatEval(longText) //corrupted payload
+        .transform(gunzip()))
+        .map(_ => 1L)
+        .onErrorFallbackTo(Observable.raiseError(ex))
+      Sample(o, sourceCount + 1, sourceCount + 1, Zero, Zero)
+    }
+
+  override def observableInError(sourceCount: Int, ex: Throwable): Option[GunzipOperatorSuite.Sample] =
+    Some {
+      val o = createObservableEndingInError(
+        Observable
+          .repeatEval(jdkGzip(longText, syncFlush = false))
+          .take(sourceCount.toLong - 1)
+          .transform(gunzip(64))
+          .map(_ => 1L),
+        ex)
+      Sample(o, sourceCount, sourceCount, Zero, Zero)
+    }
+
+  override def cancelableObservables(): Seq[GunzipOperatorSuite.Sample] = Seq.empty
+}
---
+++ monix-reactive/jvm/src/test/scala/monix/reactive/compression/GzipOperatorSuite.scala
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2014-2020 by The Monix Project Developers.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monix.reactive.compression
+
+import monix.execution.Scheduler
+import monix.reactive.Observable
+import monix.reactive.internal.operators.BaseOperatorSuite
+
+import scala.concurrent.duration.Duration.Zero
+
+object GzipOperatorSuite extends BaseOperatorSuite with GzipTestsUtils {
+
+  implicit val scheduler: Scheduler =
+    Scheduler.computation(parallelism = 4, name = "compression-tests", daemonic = true)
+
+  testAsync("gzip empty bytes, small buffer") { _ =>
+    Observable
+      .empty[Array[Byte]]
+      .transform(gzip(1))
+      .toListL
+      .map(l => assert(jdkGunzip(l.flatten.toArray).isEmpty))
+      .runToFuture
+  }
+  testAsync("gzip empty bytes") { _ =>
+    Observable
+      .empty[Array[Byte]]
+      .transform(gzip(`1K`))
+      .toListL
+      .map(l => assert(jdkGunzip(l.flatten.toArray).isEmpty))
+      .runToFuture
+  }
+  testAsync("gzips, small chunks, small buffer") { _ =>
+    Observable
+      .fromIterable(longText)
+      .bufferTumbling(1)
+      .map(_.toArray)
+      .transform(gzip(1))
+      .toListL
+      .map(l => assertArrayEquals(jdkGunzip(l.flatten.toArray), longText))
+      .runToFuture
+  }
+  testAsync("gzips, small chunks, 1k buffer") { _ =>
+    Observable
+      .fromIterable(longText)
+      .bufferTumbling(1)
+      .map(_.toArray)
+      .transform(gzip(`1K`))
+      .toListL
+      .map(l => assertArrayEquals(jdkGunzip(l.flatten.toArray), longText))
+      .runToFuture
+  }
+  testAsync("chunks bigger than buffer") { _ =>
+    Observable
+      .fromIterable(longText)
+      .bufferTumbling(`1K`)
+      .map(_.toArray)
+      .transform(gzip(64))
+      .toListL
+      .map(l => assertArrayEquals(jdkGunzip(l.flatten.toArray), longText))
+      .runToFuture
+  }
+
+  private def assertArrayEquals[T](a1: Array[T], a2: Array[T]): Unit = {
+    assertEquals(a1.toList, a2.toList)
+  }
+
+  override def createObservable(sourceCount: Int): Option[GzipOperatorSuite.Sample] =
+    Some {
+      val o = Observable
+        .repeatEval(longText)
+        .take(sourceCount.toLong - 1)
+        .transform(gzip())
+        .map(_ => 1L)
+      Sample(o, sourceCount, sourceCount, Zero, Zero)
+    }
+
+  override def brokenUserCodeObservable(sourceCount: Int, ex: Throwable): Option[GzipOperatorSuite.Sample] = None
+
+  override def observableInError(sourceCount: Int, ex: Throwable): Option[GzipOperatorSuite.Sample] =
+    Some {
+      val o = createObservableEndingInError(
+        Observable
+          .repeatEval(longText)
+          .take(sourceCount.toLong - 1)
+          .transform(gzip())
+          .map(_ => 1L),
+        ex)
+      Sample(o, sourceCount, sourceCount, Zero, Zero)
+    }
+
+  override def cancelableObservables(): Seq[GzipOperatorSuite.Sample] = Seq.empty
+}
