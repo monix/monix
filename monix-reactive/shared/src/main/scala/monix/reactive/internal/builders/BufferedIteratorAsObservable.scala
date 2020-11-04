0067475b22fd39410522ad8f336be10550e17bba
Avasil <pgawrys2@gmail.com>
2020-11-04 11:10:23 +0100

    Add Observable.fromIteratorChunked

---
+++ monix-reactive/shared/src/main/scala/monix/reactive/internal/builders/BufferedIteratorAsObservable.scala
@@ -0,0 +1,184 @@
+/*
+ * Copyright (c) 2014-2020 by The Monix Project Developers.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monix.reactive.internal.builders
+
+import monix.execution.Ack.{Continue, Stop}
+import monix.execution.atomic.Atomic
+import monix.execution.cancelables.BooleanCancelable
+import monix.execution.exceptions.APIContractViolationException
+import monix.execution.{Ack, Cancelable, ExecutionModel, Scheduler}
+import monix.reactive.Observable
+import monix.reactive.observers.Subscriber
+
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.Future
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success}
+
+private[reactive] final class BufferedIteratorAsObservable[A](iterator: Iterator[A], bufferSize: Int)
+  extends Observable[Seq[A]] {
+  require(bufferSize > 0, "chunkSize must be strictly positive")
+
+  private[this] val wasSubscribed = Atomic(false)
+
+  def unsafeSubscribeFn(out: Subscriber[Seq[A]]): Cancelable = {
+    if (wasSubscribed.getAndSet(true)) {
+      out.onError(APIContractViolationException("InputStreamObservable"))
+      Cancelable.empty
+    } else {
+      startLoop(out)
+    }
+  }
+
+  private def startLoop(subscriber: Subscriber[Seq[A]]): Cancelable = {
+    import subscriber.{scheduler => s}
+    // Protect against contract violations - we are only allowed to
+    // call onError if no other terminal event has been called.
+    var streamErrors = true
+    try {
+      val iteratorHasNext = iterator.hasNext
+      streamErrors = false
+      // Short-circuiting empty iterators, as there's no reason to
+      // start the streaming if we have no elements
+      if (!iteratorHasNext) {
+        subscriber.onComplete()
+        Cancelable.empty
+      } else {
+        // Starting the synchronous loop
+        val cancelable = BooleanCancelable()
+        fastLoop(iterator, subscriber, cancelable, s.executionModel, 0)(s)
+        cancelable
+      }
+    } catch {
+      case ex if NonFatal(ex) =>
+        // We can only stream onError events if we have a guarantee
+        // that no other final events happened, otherwise we could
+        // violate the contract.
+        if (streamErrors) {
+          subscriber.onError(ex)
+          Cancelable.empty
+        } else {
+          s.reportFailure(ex)
+          Cancelable.empty
+        }
+    }
+  }
+
+  /** In case of an asynchronous boundary, we reschedule the the
+    * run-loop on another logical thread. Usage of `onComplete` takes
+    * care of that.
+    *
+    * NOTE: the assumption of this method is that `iter` is
+    * NOT empty, so the first call is `next()` and not `hasNext()`.
+    */
+  private def reschedule(
+    ack: Future[Ack],
+    iter: Iterator[A],
+    out: Subscriber[Seq[A]],
+    c: BooleanCancelable,
+    em: ExecutionModel)(implicit s: Scheduler): Unit = {
+
+    ack.onComplete {
+      case Success(next) =>
+        if (next == Continue)
+          // If fastLoop throws, then it's a contract violation and
+          // the only thing we can do is to log it
+          try {
+            fastLoop(iter, out, c, em, 0)
+          } catch {
+            case ex if NonFatal(ex) =>
+              // Protocol violation
+              s.reportFailure(ex)
+          }
+      case Failure(ex) =>
+        out.onError(ex)
+    }
+  }
+
+  /** The `fastLoop` is a tail-recursive function that goes through the
+    * elements of our iterator, one by one, and tries to push them
+    * synchronously, for as long as the `ExecutionModel` permits.
+    *
+    * After it encounters an asynchronous boundary (i.e. an
+    * uncompleted `Future` returned by `onNext`), then we
+    * [[reschedule]] the loop on another logical thread.
+    *
+    * NOTE: the assumption of this method is that `iter` is
+    * NOT empty, so the first call is `next()` and not `hasNext()`.
+    */
+  @tailrec private def fastLoop(
+    iter: Iterator[A],
+    out: Subscriber[Seq[A]],
+    c: BooleanCancelable,
+    em: ExecutionModel,
+    syncIndex: Int)(implicit s: Scheduler): Unit = {
+    // The result of onNext calls, on which we must do back-pressure
+    var ack: Future[Ack] = Continue
+    // We do not want to catch errors from our interaction with our
+    // observer, since SafeObserver should take care of than, hence we
+    // must only catch and stream errors related to the interactions
+    // with the iterator
+    var streamErrors = true
+    // True in case our iterator is seen to be empty and we must
+    // signal onComplete
+    var iteratorHasNext = true
+    // non-null in case we caught an iterator related error and we
+    // must signal onError
+    var iteratorTriggeredError: Throwable = null
+
+    val buffer = ListBuffer.empty[A]
+    var size = 0
+
+    try {
+      while (iteratorHasNext && size < bufferSize) {
+        val next = iter.next()
+        buffer.append(next)
+        size += 1
+        iteratorHasNext = iter.hasNext
+      }
+
+      ack = out.onNext(buffer.toList)
+    } catch {
+      case NonFatal(ex) if streamErrors =>
+        iteratorTriggeredError = ex
+    }
+
+    // Signaling onComplete
+    if (iteratorTriggeredError != null) {
+      // Signaling error only if the subscription isn't canceled
+      if (!c.isCanceled) out.onError(iteratorTriggeredError)
+      else s.reportFailure(iteratorTriggeredError)
+    } else if (!iteratorHasNext) {
+      streamErrors = true
+      // Iterator has finished
+      out.onComplete()
+    } else {
+      // Logic for collapsing execution loops
+      val nextIndex =
+        if (ack == Continue) em.nextFrameIndex(syncIndex) // TODO check if its fine to add batch size
+        else if (ack == Stop) -1
+        else 0
+
+      if (nextIndex > 0)
+        fastLoop(iter, out, c, em, nextIndex)
+      else if (nextIndex == 0 && !c.isCanceled)
+        reschedule(ack, iter, out, c, em)
+    }
+  }
+}
---
+++ monix-reactive/shared/src/test/scala/monix/reactive/internal/builders/BufferedIteratorAsObservableSuite.scala
@@ -0,0 +1,353 @@
+/*
+ * Copyright (c) 2014-2020 by The Monix Project Developers.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monix.reactive.internal.builders
+
+import cats.effect.Resource
+import minitest.TestSuite
+import monix.eval.Task
+import monix.execution.Ack.Continue
+import monix.execution.exceptions.APIContractViolationException
+import monix.execution.{Ack, Scheduler}
+import monix.execution.schedulers.TestScheduler
+import monix.reactive.Observable
+import monix.execution.exceptions.DummyException
+import monix.reactive.observers.Subscriber
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+object BufferedIteratorAsObservableSuite extends TestSuite[TestScheduler] {
+  def setup() = TestScheduler()
+  def tearDown(s: TestScheduler) = {
+    assert(s.state.tasks.isEmpty, "TestScheduler should be left with no pending tasks")
+  }
+
+  test("yields a single subscriber observable") { implicit s =>
+    var errorThrown: Throwable = null
+    val obs = Observable.fromIteratorBufferedUnsafe(Seq(1, 2, 3).iterator, 1)
+    obs.unsafeSubscribeFn(Subscriber.empty(s))
+
+    obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler = s
+
+      def onNext(elem: Seq[Int]): Ack =
+        throw new IllegalStateException("onNext")
+      def onComplete(): Unit =
+        throw new IllegalStateException("onComplete")
+      def onError(ex: Throwable): Unit =
+        errorThrown = ex
+    })
+
+    assert(errorThrown.isInstanceOf[APIContractViolationException])
+  }
+
+  test("fromIterator(resource) should call finalizer") { implicit s =>
+    var onFinishCalled = 0
+    var onCompleteCalled = 0
+    var sum = 0
+
+    val n = s.executionModel.recommendedBatchSize * 4
+    val seq = 0 until n
+    val obs = Observable
+      .fromIteratorBuffered(Resource.make(Task(seq.iterator))(_ => Task { onFinishCalled += 1 }), 1)
+      .map { x =>
+        assertEquals(onFinishCalled, 0); x
+      }
+
+    obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler: Scheduler = s
+
+      def onNext(elem: Seq[Int]): Ack = { sum += elem.sum; Continue }
+      def onComplete(): Unit =
+        onCompleteCalled += 1
+      def onError(ex: Throwable): Unit =
+        throw new IllegalStateException("onError")
+    })
+
+    s.tick()
+    assertEquals(s.state.lastReportedError, null)
+    assertEquals(onCompleteCalled, 1)
+    assertEquals(onFinishCalled, 1)
+    assertEquals(sum, n * (n - 1) / 2)
+  }
+
+  test("fromIterator(resource) should back-pressure onNext before calling finalizer") { implicit s =>
+    var onFinishCalled = 0
+    var onCompleteCalled = 0
+    var sum = 0
+
+    val n = s.executionModel.recommendedBatchSize * 4
+    val seq = 0 until n
+    val obs = Observable
+      .fromIteratorBuffered(Resource.make(Task(seq.iterator))(_ => Task { onFinishCalled += 1 }), 1)
+      .mapEval(x => Task(assertEquals(onFinishCalled, 0)).map(_ => x).delayExecution(1.millis))
+
+    obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler: Scheduler = s
+
+      def onNext(elem: Seq[Int]): Ack = { sum += elem.sum; Continue }
+      def onComplete(): Unit = onCompleteCalled += 1
+      def onError(ex: Throwable): Unit =
+        throw new IllegalStateException("onError")
+    })
+
+    s.tick(1.millis * n.toLong)
+    assertEquals(sum, n * (n - 1) / 2)
+    assertEquals(onCompleteCalled, 1)
+    assertEquals(onFinishCalled, 1)
+  }
+
+  test("onFinish should be called upon onError") { implicit s =>
+    val ex = DummyException("dummy")
+    var onFinishCalled = 0
+    var onErrorCalled: Throwable = null
+    var sum = 0
+
+    val n = s.executionModel.recommendedBatchSize * 4
+    val seq = 0 until n
+    val obs = Observable
+      .fromIteratorBuffered(Resource.make(Task(seq.iterator))(_ => Task { onFinishCalled += 1 }), 1)
+      .endWithError(ex)
+
+    obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler: Scheduler = s
+
+      def onNext(elem: Seq[Int]): Ack = { sum += elem.sum; Continue }
+      def onComplete(): Unit =
+        throw new IllegalStateException("onComplete")
+      def onError(ex: Throwable): Unit =
+        onErrorCalled = ex
+    })
+
+    s.tick()
+    assertEquals(sum, n * (n - 1) / 2)
+    assertEquals(onErrorCalled, ex)
+    assertEquals(onFinishCalled, 1)
+  }
+
+  test("onFinish should be called upon Stop") { implicit s =>
+    var onFinishCalled = 0
+    var onCompleteCalled = 0
+    var sum = 0
+
+    val n = s.executionModel.recommendedBatchSize * 4
+    val seq = 0 until (n * 2)
+    val obs = Observable
+      .fromIteratorBuffered(Resource.make(Task(seq.iterator))(_ => Task { onFinishCalled += 1 }), 1)
+      .take(n.toLong) // Will trigger Stop
+
+    obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler: Scheduler = s
+
+      def onNext(elem: Seq[Int]): Ack = { sum += elem.sum; Continue }
+      def onComplete(): Unit = onCompleteCalled += 1
+      def onError(ex: Throwable): Unit =
+        throw new IllegalStateException("onError")
+    })
+
+    s.tick()
+    assertEquals(sum, n * (n - 1) / 2)
+    assertEquals(onCompleteCalled, 1)
+    assertEquals(onFinishCalled, 1)
+  }
+
+  test("onFinish should be called upon subscription cancel") { implicit s =>
+    var onFinishCalled = 0
+    var onCompleteCalled = 0
+    var received = 0
+
+    val n = s.executionModel.recommendedBatchSize
+    val seq = 0 until (n * 4)
+    val obs = Observable.fromIteratorBuffered(Resource.make(Task(seq.iterator))(_ => Task { onFinishCalled += 1 }), 1)
+
+    val c = obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler: Scheduler = s
+
+      def onNext(elem: Seq[Int]): Ack = { received += elem.size; Continue }
+      def onComplete(): Unit = onCompleteCalled += 1
+      def onError(ex: Throwable): Unit =
+        throw new IllegalStateException("onError")
+    })
+
+    c.cancel()
+    s.tick()
+
+    assertEquals(received, n * 2)
+    assertEquals(onCompleteCalled, 0)
+    assertEquals(onFinishCalled, 1)
+  }
+
+  test("onFinish should be called if onNext triggers error before boundary") { implicit s =>
+    val ex = DummyException("dummy")
+    var onFinishCalled = 0
+    var received = 0
+    var wasThrown: Throwable = null
+
+    val n = s.executionModel.recommendedBatchSize
+    val seq = 0 until (n * 4)
+    val obs = Observable.fromIteratorBuffered(Resource.make(Task(seq.iterator))(_ => Task { onFinishCalled += 1 }), 1)
+
+    obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler: Scheduler = s
+
+      def onNext(elem: Seq[Int]): Ack = {
+        received += elem.size
+        if (received == n) throw ex
+        Continue
+      }
+
+      def onComplete(): Unit =
+        throw new IllegalStateException("onComplete")
+      def onError(ex: Throwable): Unit =
+        wasThrown = ex
+    })
+
+    s.tick()
+    assertEquals(received, n)
+    assertEquals(onFinishCalled, 1)
+    assertEquals(wasThrown, ex)
+  }
+
+  test("onFinish should be called if onNext triggers error after boundary") { implicit s =>
+    val ex = DummyException("dummy")
+    var onFinishCalled = 0
+    var received = 0
+    var wasThrown: Throwable = null
+
+    val n = s.executionModel.recommendedBatchSize
+    val seq = 0 until (n * 4)
+    val obs = Observable.fromIteratorBuffered(Resource.make(Task(seq.iterator))(_ => Task { onFinishCalled += 1 }), 1)
+
+    obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler: Scheduler = s
+
+      def onNext(elem: Seq[Int]): Ack = {
+        received += elem.size
+        if (received == n * 2) throw ex
+        Continue
+      }
+
+      def onComplete(): Unit =
+        throw new IllegalStateException("onComplete")
+      def onError(ex: Throwable): Unit =
+        wasThrown = ex
+    })
+
+    s.tick()
+    assertEquals(received, n * 2)
+    assertEquals(onFinishCalled, 1)
+    assertEquals(wasThrown, ex)
+  }
+
+  test("onFinish should be called if onNext triggers error asynchronously") { implicit s =>
+    val ex = DummyException("dummy")
+    var onFinishCalled = 0
+    var received = 0
+    var wasThrown: Throwable = null
+
+    val n = s.executionModel.recommendedBatchSize
+    val seq = 0 until (n * 4)
+    val obs = Observable.fromIteratorBuffered(Resource.make(Task(seq.iterator))(_ => Task { onFinishCalled += 1 }), 1)
+
+    obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler: Scheduler = s
+
+      def onNext(elem: Seq[Int]): Future[Ack] = {
+        received += 1
+        if (received == n * 2)
+          Future.failed(ex)
+        else
+          Continue
+      }
+
+      def onComplete(): Unit =
+        throw new IllegalStateException("onComplete")
+      def onError(ex: Throwable): Unit =
+        wasThrown = ex
+    })
+
+    s.tick()
+    assertEquals(received, n * 2)
+    assertEquals(onFinishCalled, 1)
+    assertEquals(wasThrown, ex)
+  }
+
+  test("onFinish throwing just before onComplete") { implicit s =>
+    val ex = DummyException("ex")
+    var wasThrown: Throwable = null
+    var sum = 0
+
+    val n = s.executionModel.recommendedBatchSize * 4
+    val seq = 0 until n
+    val obs = Observable
+      .fromIteratorBuffered(Resource.make(Task(seq.iterator))(_ => Task.raiseError(ex)), 1)
+      .map { x =>
+        assertEquals(wasThrown, null); x
+      }
+
+    obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler: Scheduler = s
+
+      def onNext(elem: Seq[Int]): Ack = {
+        sum += elem.sum
+        Continue
+      }
+
+      def onComplete(): Unit =
+        throw new IllegalStateException("onComplete")
+      def onError(ex: Throwable): Unit =
+        wasThrown = ex
+    })
+
+    s.tick()
+    assertEquals(wasThrown, ex)
+    assertEquals(sum, n * (n - 1) / 2)
+  }
+
+  test("onFinish throwing after Stop") { implicit s =>
+    val ex = DummyException("ex")
+    var onCompleteCalled = 0
+    var received = 0
+
+    val n = s.executionModel.recommendedBatchSize
+    val seq = 0 until (n * 4)
+    val obs = Observable
+      .fromIteratorBuffered(Resource.make(Task(seq.iterator))(_ => Task.raiseError(ex)), 1)
+      .take(n.toLong)
+
+    obs.unsafeSubscribeFn(new Subscriber[Seq[Int]] {
+      implicit val scheduler: Scheduler = s
+
+      def onNext(elem: Seq[Int]): Ack = {
+        received += elem.size
+        Continue
+      }
+
+      def onComplete(): Unit =
+        onCompleteCalled += 1
+      def onError(ex: Throwable): Unit =
+        throw new IllegalStateException("onError")
+    })
+
+    s.tick()
+    assertEquals(received, n)
+    // onComplete gets called via `take`, which sends `Stop` upstream and
+    // sends onComplete downstream and there isn't much we can do here
+    assertEquals(onCompleteCalled, 1)
+    assertEquals(s.state.lastReportedError, ex)
+  }
+}
