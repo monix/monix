df254f581e4ae5bdd6e854d78eb69d450254c59a
Alexandru Nedelcu <noreply@alexn.org>
2022-05-20 23:26:30 +0300

    Fix formating

--- monix-execution/atomic/js/src/main/scala_3.0-/monix/execution/atomic/Atomic.scala
+++ monix-execution/atomic/js/src/main/scala_3.0-/monix/execution/atomic/Atomic.scala
@@ -145,7 +145,9 @@ object Atomic {
   /** Returns the builder that would be chosen to construct Atomic
     * references for the given `initialValue`.
     */
-  def builderFor[A, R <: Atomic[A]](@unused initialValue: A)(implicit builder: AtomicBuilder[A, R]): AtomicBuilder[A, R] =
+  def builderFor[A, R <: Atomic[A]](@unused initialValue: A)(implicit
+    builder: AtomicBuilder[A, R]
+  ): AtomicBuilder[A, R] =
     builder
 
   implicit final class DeprecatedExtensions[A](val self: Atomic[A]) extends AnyVal {
