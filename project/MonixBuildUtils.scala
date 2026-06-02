f074dd6e2a68e0835dcbc1f75862a11b23ca58b6
Alexandru Nedelcu <noreply@alexn.org>
2026-06-02 15:20:57 +0300

    Add crossScalaVersions directly in build.sbt

--- project/MonixBuildUtils.scala
+++ project/MonixBuildUtils.scala
@@ -1,46 +1,9 @@
 import sbt.Keys._
 import sbt._
-import sbt.io.Using
 
-import scala.annotation.tailrec
-import scala.collection.JavaConverters._
-import scala.collection.mutable
-import scala.collection.immutable.SortedSet
 import scala.util.matching.Regex
 import scala.xml.Elem
 import scala.xml.transform.{ RewriteRule, RuleTransformer }
-
-final case class MonixScalaVersion(value: String) {
-  lazy val parts =
-    value.split("[.]").filter(_.nonEmpty).toList
-
-  def filterPrefix(prefix: String): Option[MonixScalaVersion] =
-    if (value.startsWith(prefix)) Some(this) else None
-}
-
-object MonixScalaVersion {
-  implicit object Ord extends Ordering[MonixScalaVersion] {
-    override def compare(x: MonixScalaVersion, y: MonixScalaVersion): Int =
-      -1 * compare(x.parts, y.parts)
-
-    @tailrec
-    private def compare(l1: List[String], l2: List[String]): Int =
-      l1 match {
-        case h1 :: r1 =>
-          l2 match {
-            case h2 :: r2 =>
-              h1.compare(h2) match {
-                case 0 => compare(r1, r2)
-                case r => r
-              }
-            case Nil =>
-              1
-          }
-        case Nil =>
-          -1
-      }
-  }
-}
 
 final case class MonixCrossModule(
   jvm: Project => Project,
@@ -93,35 +56,4 @@ object MonixBuildUtils {
     }
   }
 
-  /** Reads the Scala versions from `.github/workflows/build.yml`.
-    */
-  def scalaVersionsFromBuildYaml(manifest: File): SortedSet[MonixScalaVersion] = {
-    Using.fileInputStream(manifest) { fis =>
-      val yaml = new org.yaml.snakeyaml.Yaml()
-        .loadAs(fis, classOf[java.util.Map[Any, Any]])
-        .asScala
-
-      val scalaVersions = yaml
-        .get("jobs")
-        .collect { case map: java.util.Map[Any, Any] @unchecked => map.asScala }
-        .getOrElse(mutable.Map.empty[Any, Any])
-        .get("jvm-tests")
-        .collect { case map: java.util.Map[Any, Any] @unchecked => map.asScala }
-        .getOrElse(mutable.Map.empty[Any, Any])
-        .get("strategy")
-        .collect { case map: java.util.Map[Any, Any] @unchecked => map.asScala }
-        .getOrElse(mutable.Map.empty[Any, Any])
-        .get("matrix")
-        .collect { case map: java.util.Map[Any, Any] @unchecked => map.asScala }
-        .getOrElse(mutable.Map.empty[Any, Any])
-        .get("scala")
-        .collect { case list: java.util.List[Any] @unchecked => list.asScala }
-        .getOrElse(Iterable.empty[Any])
-        .collect { case v: String => MonixScalaVersion(v) }
-        .toSeq
-
-      assert(scalaVersions.nonEmpty, "build.yml is corrupt, suitable scala_version_* keys missing")
-      SortedSet(scalaVersions*)
-    }
-  }
 }
