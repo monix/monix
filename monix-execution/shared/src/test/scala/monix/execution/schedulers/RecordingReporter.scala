36d371167f80a8410302ee8b1a61b68f17cbb17c
Mikołaj Bul <m.bul@avsystem.com>
2026-08-05 13:19:15 +0200

    Extend the Scheduler test suites around error reporting
    - Merged `ScheduledExecutorToSchedulerSuite` into `ExecutorSchedulerSuite` to remove duplication.
    - Extend `ExecutorSchedulerSuite` with the scheduled error-reporting and stop-after-failure cases
    - Build `ComputationSchedulerSuite`'s scheduler with `Scheduler.computation`
    - Extend `UncaughtExceptionReporterBaseSuite` with the `scheduleX` cases and close the
      `SchedulerService`-backed schedulers it builds
    - Extend `AsyncSchedulerJSSuite` with the stop-after-failure cases

---
+++ monix-execution/shared/src/test/scala/monix/execution/schedulers/RecordingReporter.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2014-2022 Monix Contributors.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package monix.execution.schedulers
+
+import monix.execution.UncaughtExceptionReporter
+import scala.concurrent.{ Future, Promise }
+
+/** An [[UncaughtExceptionReporter]] that captures the first two failures reported to it, so that a
+  * test can wait for a failure and then check that it was not reported again.
+  */
+private[schedulers] final class RecordingReporter extends UncaughtExceptionReporter {
+  private val firstThrowable = Promise[Throwable]()
+  private val secondThrowable = Promise[Throwable]()
+
+  def reportFailure(failure: Throwable): Unit = {
+    if (!firstThrowable.trySuccess(failure)) secondThrowable.trySuccess(failure)
+  }
+
+  def firstFailure: Future[Throwable] = firstThrowable.future
+  def secondFailure: Future[Throwable] = secondThrowable.future
+
+  def receivedFirstFailure: Boolean = firstThrowable.isCompleted
+  def receivedSecondFailure: Boolean = secondThrowable.isCompleted
+}
